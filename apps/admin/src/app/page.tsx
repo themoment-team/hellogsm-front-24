4732d23820e904d2dbccc4d5f64aa5de7eae4382
Lee-Seungje <moondgod@gmail.com>
2024-06-11 14:35:24 +0900

    Add button

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
-import { Test } from "shared";
+import { Test, Button } from "shared";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <Test />
+      <Button>button</Button>
     </main>
   );
 }
--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Test } from "./Test";
+export * from "./ui";
