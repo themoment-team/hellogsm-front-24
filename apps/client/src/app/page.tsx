46c4a5e2683c44b3b3fdb4550a68ab6c93dd9b67
Lee-Seungje <moondgod@gmail.com>
2024-06-12 09:19:08 +0900

    Update button

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,5 +1,12 @@
-import { Button } from "client/components/ui/button";
+import { Button } from "client/components";
+
+import { Button as BTN } from "shared";
 
 export default function Home() {
-  return <Button>button</Button>;
+  return (
+    <>
+      <BTN>button1</BTN>
+      <Button>button2</Button>
+    </>
+  );
 }
