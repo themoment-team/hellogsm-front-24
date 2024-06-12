2229c9b9ec6bb24eddbbc3347c75f1313faa1195
Lee-Seungje <moondgod@gmail.com>
2024-06-12 09:17:25 +0900

    Update postcss

--- apps/client/postcss.config.mjs
+++ apps/client/postcss.config.mjs
@@ -1,8 +1,3 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+import config from "tailwind-config/postcss.config.mjs";
 
 export default config;
