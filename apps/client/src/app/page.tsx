5428669b9bf9b92790163d2399005dc34ebc01fc
Lee-Seungje <moondgod@gmail.com>
2024-06-12 09:05:30 +0900

    Delete useless code

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 import { Button } from "client/components/ui/button";
 
 export default function Home() {
-  return <Button className="text-red-900">button</Button>;
+  return <Button>button</Button>;
 }
