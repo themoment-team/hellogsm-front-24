a05f712c303603855a24bb72187420c8d78c2a16
gaoooon <gaon9749@gmail.com>
2024-07-03 19:46:39 +0900

    Add SelectIcon

---
+++ apps/client/src/assets/SelectIcon.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  style?: string;
+}
+
+const SelectIcon: React.FC<Props> = ({ style }) => (
+  <svg
+    className={style}
+    width="11"
+    height="8"
+    viewBox="0 0 11 8"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 1L5.5 6L10 1"
+      stroke="white"
+      strokeOpacity="0.56"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default SelectIcon;
