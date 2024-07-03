3817dd3b96a9ae6ab841949c0c47a0442553c36b
gaoooon <gaon9749@gmail.com>
2024-07-03 23:51:14 +0900

    Update SemesterForm Components

--- apps/client/src/components/SemesterForm/index.tsx
+++ apps/client/src/components/SemesterForm/index.tsx
@@ -1,20 +1,12 @@
 import { UseFormRegister } from 'react-hook-form';
 
-import { ScoreSelect } from 'client/components';
+import { SelectIcon } from 'client/assets';
 import { ScoreFormType } from 'client/types';
 
 const array = [0, 1, 2, 3, 4, 5, 6, 7] as const;
+const scoreArray = ['A', 'B', 'C', 'D', 'E'] as const;
 
-const registerConvertor = {
-  '1학년 1학기': 'score1_1',
-  '1학년 2학기': 'score1_2',
-  '2학년 1학기': 'score2_1',
-  '2학년 2학기': 'score2_2',
-  '3학년 1학기': 'score3_1',
-  '3학년 2학기': 'score3_2',
-} as const;
-
-interface Props {
+interface SemesterFormProps {
   title:
     | '1학년 1학기'
     | '1학년 2학기'
@@ -22,24 +14,35 @@ interface Props {
     | '2학년 2학기'
     | '3학년 1학기'
     | '3학년 2학기';
+  id: 'score1_1' | 'score1_2' | 'score2_1' | 'score2_2' | 'score3_1';
   register: UseFormRegister<ScoreFormType>;
 }
 
-const SemesterForm: React.FC<Props> = ({ title, register }) => {
-  const registerId = registerConvertor[title];
-
-  return (
-    <div className="flex flex-col">
-      <h1 className="mb-[20px] flex h-[55px] w-[127px] items-center justify-center rounded-[6px] bg-[#0C4680] text-[17px]/[24.62px] font-[700] text-[#F8F8F8]">
-        {title}
-      </h1>
-      <div className="flex flex-col gap-[13px]">
-        {array.map((item, idx) => (
-          <ScoreSelect key={item} {...register(`${registerId}.${idx}`)} />
-        ))}
-      </div>
+const SemesterForm: React.FC<SemesterFormProps> = ({ title, id, register }) => (
+  <div className="flex flex-col">
+    <h1 className="mb-[20px] flex h-[55px] w-[127px] items-center justify-center rounded-[6px] bg-[#0C4680] text-[17px]/[24.62px] font-[700] text-[#F8F8F8]">
+      {title}
+    </h1>
+    <div className="flex flex-col gap-[13px]">
+      {array.map((item, idx) => (
+        <div className="relative z-[1] rounded-[6px] bg-[#484453]" key={item}>
+          <select
+            className="relative z-[3] flex h-[37px] w-full cursor-pointer select-none appearance-none rounded-[6px] bg-transparent text-center text-[17px]/[24.62px] font-[500] text-[#FFFFFF8F]/[0.54]"
+            defaultValue="선택"
+            {...register(`${id}.${idx}`)}
+          >
+            <option hidden>선택</option>
+            {scoreArray.map((score, idx) => (
+              <option key={score} value={5 - idx}>
+                {score}
+              </option>
+            ))}
+          </select>
+          <SelectIcon style="absolute right-5 top-1/2 z-[2] -translate-y-1/2 transform" />
+        </div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default SemesterForm;
