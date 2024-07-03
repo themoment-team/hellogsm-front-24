0947d24122291063f3bf7cc5c46d0fe166c41504
gaoooon <gaon9749@gmail.com>
2024-07-03 19:51:31 +0900

    Add TestCalculatePage

--- apps/client/src/components/TestCalculatePage/index.tsx
+++ apps/client/src/components/TestCalculatePage/index.tsx
@@ -1,45 +1,38 @@
 'use client';
 
-import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { ScoreSelect } from 'client/components';
-
-const array = [0, 1, 2, 3, 4, 5, 6, 7];
-
-interface ScoreForm {
-  score1_1: string[];
-  score1_2: string[];
-  score2_1: string[];
-  score2_2: string[];
-  score3_1: string[];
-}
+import { SemesterForm } from 'client/components';
+import { scoreFormSchema } from 'client/schemas';
+import { ScoreFormType } from 'client/types';
 
 const TestCalculatePage = () => {
-  const { register, handleSubmit } = useForm<ScoreForm>({});
+  const { register, handleSubmit } = useForm<ScoreFormType>({
+    resolver: zodResolver(scoreFormSchema),
+  });
 
-  const handleFormSubmit = (data) => {
+  const handleFormSubmit: SubmitHandler<ScoreFormType> = (data) => {
     console.log(data);
   };
 
   return (
-    <div className="h-lvh">
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-        {array.map((item, index) => (
-          <ScoreSelect key={item} {...register(`score1_1.${index}`)} />
-        ))}
-        {array.map((item, index) => (
-          <ScoreSelect key={item} {...register(`score1_2.${index}`)} />
-        ))}
-        {array.map((item, index) => (
-          <ScoreSelect key={item} {...register(`score2_1.${index}`)} />
-        ))}
-        {array.map((item, index) => (
-          <ScoreSelect key={item} {...register(`score2_2.${index}`)} />
-        ))}
-        {array.map((item, index) => (
-          <ScoreSelect key={item} {...register(`score3_1.${index}`)} />
-        ))}
-      </form>
+    <div className="flex h-lvh items-center justify-center">
+      <div className="flex flex-col items-center">
+        <form className="flex gap-6">
+          <SemesterForm title="1학년 1학기" register={register} />
+          <SemesterForm title="1학년 2학기" register={register} />
+          <SemesterForm title="2학년 1학기" register={register} />
+          <SemesterForm title="2학년 2학기" register={register} />
+          <SemesterForm title="3학년 1학기" register={register} />
+        </form>
+        <button
+          onClick={handleSubmit(handleFormSubmit)}
+          className="mt-[100px] rounded-[10px] border border-[#0F0921] px-[87.5px] py-[10px] text-[28px]/[40.54px] font-[700] text-[#0F0921]"
+        >
+          저장
+        </button>
+      </div>
     </div>
   );
 };
