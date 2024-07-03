8d32c86668796f21c8804eafa5a50e0720efb42c
gaoooon <gaon9749@gmail.com>
2024-07-03 23:50:17 +0900

    Delete ScoreSelect Component

--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -1,4 +1,3 @@
 export * from './ui';
-export { default as ScoreSelect } from './ScoreSelect';
 export { default as SemesterForm } from './SemesterForm';
 export { default as TestCalculatePage } from './TestCalculatePage';
