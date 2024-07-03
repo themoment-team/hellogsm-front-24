4aff00b2462801abb7603be8145856477e2e7072
gaoooon <gaon9749@gmail.com>
2024-07-03 19:49:25 +0900

    Add ScoreSelect Component

--- apps/client/src/components/ScoreSelect/index.tsx
+++ apps/client/src/components/ScoreSelect/index.tsx
@@ -1,31 +1,19 @@
 import { forwardRef } from 'react';
 
-import { SelectIcon } from 'client/assets';
-
 const scoreArray = ['A', 'B', 'C', 'D', 'E'] as const;
 
-interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {}
-
-const ScoreSelect = forwardRef<HTMLSelectElement, Props>(({ ...attributes }, ref) => {
+const ScoreSelect = forwardRef<HTMLSelectElement>(({}, ref) => {
   return (
-    <div className="relative">
-      {/* <label htmlFor="select"></label> */}
-      <select
-        id="select"
-        {...attributes}
-        ref={ref}
-        className="flex h-[37px] w-full cursor-pointer appearance-none rounded-[6px] bg-[#484453] text-center text-[17px]/[24.62px] font-[500] text-[#FFFFFF8F]/[0.54]"
-        defaultValue="선택"
-      >
-        <option hidden>선택</option>
-        {scoreArray.map((score, idx) => (
-          <option key={score} value={5 - idx}>
-            {score}
-          </option>
-        ))}
-      </select>
-      <SelectIcon style="absolute top-1/2 right-5 transform -translate-y-1/2" />
-    </div>
+    <select ref={ref}>
+      <option hidden selected>
+        선택
+      </option>
+      {scoreArray.map((score, index) => (
+        <option value={5 - (index - 1)} key={score}>
+          {score}
+        </option>
+      ))}
+    </select>
   );
 });
 
