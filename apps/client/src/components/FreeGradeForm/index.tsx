3909b77fef03e7ecbbdb7c79f81f32ee9d5a92e8
gaoooon <gaon9749@gmail.com>
2024-07-13 00:41:31 +0900

    Add FreeGradeForm Component

---
+++ apps/client/src/components/FreeGradeForm/index.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @rushstack/no-new-null */
+
+import { UseFormRegister } from 'react-hook-form';
+
+import { SemesterForm } from 'client/components';
+import { ScoreFormType, SemesterId, SemesterType } from 'client/types';
+
+const freeGradeArray: { title: SemesterType; id: SemesterId }[] = [
+  { title: '2학년 1학기', id: 'score2_1' },
+  { title: '2학년 2학기', id: 'score2_2' },
+  { title: '3학년 1학기', id: 'score3_1' },
+] as const;
+
+interface FreeGradeFormProps {
+  register: UseFormRegister<ScoreFormType>;
+  subjectArray: string[];
+}
+
+const FreeGradeForm: React.FC<FreeGradeFormProps> = ({ register, subjectArray }) => {
+  return (
+    <>
+      {freeGradeArray.map(({ id, title }) => (
+        <SemesterForm
+          key={id}
+          title={title}
+          id={id}
+          register={register}
+          subjectArray={subjectArray}
+        />
+      ))}
+    </>
+  );
+};
+
+export default FreeGradeForm;
