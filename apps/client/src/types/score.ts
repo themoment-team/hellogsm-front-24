5ac878969cfef523f955103c72b637490b7678c4
gaoooon <gaon9749@gmail.com>
2024-07-13 00:40:14 +0900

    Add GradesInputMethodType

--- apps/client/src/types/score.ts
+++ apps/client/src/types/score.ts
@@ -2,4 +2,6 @@ import { z } from 'zod';
 
 import { scoreFormSchema } from 'client/schemas';
 
+export type GradesInputMethodType = 'freeSemester' | 'freeGrade';
+
 export type ScoreFormType = z.infer<typeof scoreFormSchema>;
