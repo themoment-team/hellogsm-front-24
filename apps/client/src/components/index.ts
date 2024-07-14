69e0457ce28833eb8d6c8e45d85580d4055a78d9
gaoooon <gaon9749@gmail.com>
2024-07-14 20:04:58 +0900

    Updatre index.ts

--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -1,5 +1,4 @@
 export * from './ui';
 export { default as FreeGradeForm } from './FreeGradeForm';
 export { default as FreeSemesterForm } from './FreeSemesterForm';
-export { default as SemesterForm } from './SemesterForm';
 export { default as TestCalculatePage } from './TestCalculatePage';
