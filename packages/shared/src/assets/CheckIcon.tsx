b9056cdbfd4ff3fa4ba7f7b207b92b510954fa06
Lee-Seungje <moondgod@gmail.com>
2024-07-18 17:27:09 +0900

    feat: checkIcon 추가

---
+++ packages/shared/src/assets/CheckIcon.tsx
@@ -0,0 +1,13 @@
+const CheckIcon = () => (
+  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M11.6666 3.5L5.24992 9.91667L2.33325 7"
+      stroke="#94A3B8"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default CheckIcon;
