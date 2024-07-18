be1f9a50a031ee02f494077e9ecc2a60a3d2bdfc
Lee-Seungje <moondgod@gmail.com>
2024-07-18 21:05:30 +0900

    design: 텍스트 컬러 변경

--- packages/shared/src/components/ui/Table/index.tsx
+++ packages/shared/src/components/ui/Table/index.tsx
@@ -95,16 +95,19 @@ const Example = () => (
   <Table>
     <TableBody>
       <TableRow>
-        <TableCell className="w-[100px]">0189</TableCell>
+        <TableCell className="w-[100px] text-zinc-900">0189</TableCell>
         <TableCell className="w-[130px]">
           <Toggle icon={<CheckIcon />}>제출 완료</Toggle>
         </TableCell>
-        <TableCell className="w-[154px]">진예원</TableCell>
-        <TableCell className="w-[154px]">대성여자중학교</TableCell>
-        <TableCell className="max-w-full">일반전형</TableCell>
+        <TableCell className="w-[154px] font-semibold text-zinc-900">
+          진예원 <br />
+          <span className="font-normal text-zinc-600">010 1234 5678</span>
+        </TableCell>
+        <TableCell className="w-[154px] text-zinc-600">대성여자중학교</TableCell>
+        <TableCell className="max-w-full text-zinc-900">일반전형</TableCell>
         <TableCell className="w-[96px]">미정</TableCell>
-        <TableCell className="w-[180px]">진행 전</TableCell>
-        <TableCell className="w-[180px]">진행 전</TableCell>
+        <TableCell className="w-[180px]  text-zinc-400">진행 전</TableCell>
+        <TableCell className="w-[180px]  text-zinc-400">진행 전</TableCell>
         <TableCell className="w-[96px]">미정</TableCell>
         <TableCell className="w-[149px] ">
           <Button className="ml-[45px]" variant="outline">
