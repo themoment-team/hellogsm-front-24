992fe62d95f52783008dd042fd5409e2b845445c
Lee-Seungje <moondgod@gmail.com>
2024-07-18 16:51:14 +0900

    feat: toggle 스타일링 수정

--- packages/shared/src/components/ui/Toggle/index.stories.tsx
+++ packages/shared/src/components/ui/Toggle/index.stories.tsx
@@ -16,3 +16,26 @@ export const Primary: Story = {
     children: "test",
   },
 };
+
+export const Icon: Story = {
+  args: {
+    children: "제출 완료",
+    icon: (
+      <svg
+        width="14"
+        height="14"
+        viewBox="0 0 14 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M11.6666 3.5L5.24992 9.91667L2.33325 7"
+          stroke="#94A3B8"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    ),
+  },
+};
--- packages/shared/src/components/ui/Toggle/index.tsx
+++ packages/shared/src/components/ui/Toggle/index.tsx
@@ -3,11 +3,10 @@
 import * as React from "react";
 import * as TogglePrimitive from "@radix-ui/react-toggle";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "shared/lib/utils";
 
 const toggleVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground",
+  "gap-x-1.5 rounded-3xl border-slate-200 border text-slate-400 inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors hover:border-emerald-500 hover:bg-muted hover:text-emerald-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-emerald-500 data-[state=on]:border-emerald-500",
   {
     variants: {
       variant: {
@@ -15,7 +14,7 @@ const toggleVariants = cva(
         outline: "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground",
       },
       size: {
-        default: "h-10 px-3",
+        default: "px-2.5 py-1",
         sm: "h-9 px-2.5",
         lg: "h-11 px-5",
       },
@@ -29,13 +28,19 @@ const toggleVariants = cva(
 
 const Toggle = React.forwardRef<
   React.ElementRef<typeof TogglePrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> & VariantProps<typeof toggleVariants>
->(({ className, variant, size, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
+    VariantProps<typeof toggleVariants> & { icon?: React.ReactNode }
+>(({ className, variant, size, icon, ...props }, ref) => (
   <TogglePrimitive.Root
     ref={ref}
-    className={cn(toggleVariants({ variant, size, className }))}
+    className={cn(toggleVariants({ variant, size }), className, "toggle-icon-hover", {
+      "toggle-icon-active": !!icon,
+    })}
     {...props}
-  />
+  >
+    {icon && <span className="toggle-icon">{icon}</span>}
+    {props.children}
+  </TogglePrimitive.Root>
 ));
 
 Toggle.displayName = TogglePrimitive.Root.displayName;
