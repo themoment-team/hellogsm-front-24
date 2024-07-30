e6f96f122437b1fec0fb7107258f0e81cadc1f28
Lee-Seungje <moondgod@gmail.com>
2024-07-30 19:27:27 +0900

    refactor: formatScore 수정

--- packages/shared/src/utils/formatScore.ts
+++ packages/shared/src/utils/formatScore.ts
@@ -1,21 +1,20 @@
 // 입력받은 점수에서 숫자만 추출합니다. 점수가 maxScore보다 클 경우, 뒷 자리를 제거합니다.
 
-const formatScore = (score: string, maxScore?: number) => {
-  maxScore = maxScore ?? 100;
-
-  const onlyNumberScore = parseInt(
+const formatScore = (score: string, maxScore: number = 100): string => {
+  // 점수에서 숫자와 소숫점만 추출
+  const onlyNumberScore = parseFloat(
     score
       .split("")
-      .filter((v) => !isNaN(parseInt(v)))
+      .filter((v) => !isNaN(parseInt(v)) || v === ".")
       .join(""),
   );
 
-  let scoreResult;
+  // 소숫점 2자리로 자르고 maxScore와 비교
+  let scoreResult = Math.min(onlyNumberScore, maxScore).toFixed(2);
 
-  if (onlyNumberScore <= maxScore) {
-    scoreResult = onlyNumberScore.toString();
-  } else {
-    scoreResult = onlyNumberScore.toString().slice(0, 1);
+  // 소숫점이 없을 경우 .00을 붙임
+  if (!scoreResult.includes(".")) {
+    scoreResult += ".00";
   }
 
   return scoreResult;
