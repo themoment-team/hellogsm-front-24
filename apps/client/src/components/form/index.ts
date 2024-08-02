274f23bea693f726589f965f9e15821c12b9de84
gaoooon <gaon9749@gmail.com>
2024-08-02 18:40:25 +0900

    Add NonSubjectForm, ArtPhysicalForm component

---
+++ apps/client/src/components/form/index.ts
@@ -0,0 +1,4 @@
+export { default as ArtPhysicalForm } from './ArtPhysicalForm';
+export { default as FreeGradeForm } from './FreeGradeForm';
+export { default as FreeSemesterForm } from './FreeSemesterForm';
+export { default as NonSubjectForm } from './NonSubjectForm';
