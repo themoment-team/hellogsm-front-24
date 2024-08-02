8c2c2939a19c53eae27773e6f003884f69213dbc
gaoooon <gaon9749@gmail.com>
2024-08-02 18:40:57 +0900

    Add artPhysicalSubject, nonSubject Array

--- apps/client/src/constants/subject.ts
+++ apps/client/src/constants/subject.ts
@@ -1,3 +1,5 @@
+export const artPhysicalSubjectArray = ['체육', '미술', '음악'] as const;
+
 export const defaultSubjectArray = [
   '국어',
   '도덕',
@@ -8,3 +10,5 @@ export const defaultSubjectArray = [
   '기술가정',
   '영어',
 ] as const;
+
+export const nonSubjectArray = ['결석', '지각', '조퇴', '결과', '봉사활동(시간)'] as const;
