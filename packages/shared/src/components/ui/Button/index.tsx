90867762a42621a6e9bd0575e08378443b0ae5a6
Lee-Seungje <moondgod@gmail.com>
2024-08-03 20:43:57 +0900

    style: eslint shared에도 적용

--- packages/shared/src/components/ui/Button/index.tsx
+++ packages/shared/src/components/ui/Button/index.tsx
@@ -1,58 +1,58 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from "shared/lib/utils";
+import { cn } from 'shared/lib/utils';
 
 const buttonVariants = cva(
   cn(
-    "inline-flex",
-    "items-center",
-    "justify-center",
-    "whitespace-nowrap",
-    "rounded-md",
-    "text-sm",
-    "font-medium",
-    "ring-offset-background",
-    "transition-colors",
-    "focus-visible:outline-none",
-    "focus-visible:ring-2",
-    "focus-visible:ring-ring",
-    "focus-visible:ring-offset-2",
-    "disabled:pointer-events-none",
-    "disabled:opacity-50",
+    'inline-flex',
+    'items-center',
+    'justify-center',
+    'whitespace-nowrap',
+    'rounded-md',
+    'text-sm',
+    'font-medium',
+    'ring-offset-background',
+    'transition-colors',
+    'focus-visible:outline-none',
+    'focus-visible:ring-2',
+    'focus-visible:ring-ring',
+    'focus-visible:ring-offset-2',
+    'disabled:pointer-events-none',
+    'disabled:opacity-50',
   ),
   {
     variants: {
       variant: {
-        default: cn("bg-slate-800", "text-white", "hover:bg-slate-200"),
-        fill: cn("bg-blue-600", "text-white", "hover:bg-blue-800"),
-        blue: cn("bg-blue-600", "text-white", "hover:bg-blue-800"),
-        destructive: cn("bg-red-500", "text-destructive-foreground", "hover:bg-red-600"),
+        default: cn('bg-slate-800', 'text-white', 'hover:bg-slate-200'),
+        fill: cn('bg-blue-600', 'text-white', 'hover:bg-blue-800'),
+        blue: cn('bg-blue-600', 'text-white', 'hover:bg-blue-800'),
+        destructive: cn('bg-red-500', 'text-destructive-foreground', 'hover:bg-red-600'),
         outline: cn(
-          "border-slate-200",
-          "border",
-          "border-input",
-          "text-slate-800",
-          "hover:bg-accent",
-          "hover:text-accent-foreground",
+          'border-slate-200',
+          'border',
+          'border-input',
+          'text-slate-800',
+          'hover:bg-accent',
+          'hover:text-accent-foreground',
         ),
-        subtitle: cn("bg-slate-100", "text-secondary-foreground", "hover:bg-slate-200"),
-        ghost: cn("hover:bg-accent", "hover:bg-slate-100"),
-        link: cn("text-primary", "underline-offset-4", "hover:underline"),
+        subtitle: cn('bg-slate-100', 'text-secondary-foreground', 'hover:bg-slate-200'),
+        ghost: cn('hover:bg-accent', 'hover:bg-slate-100'),
+        link: cn('text-primary', 'underline-offset-4', 'hover:underline'),
       },
       size: {
-        default: cn("h-10", "px-4", "py-2"),
-        sm: cn("rounded-md", "px-3", "py-2"),
-        lg: cn("h-11", "rounded-md", "px-8"),
-        icon: cn("h-10", "w-10"),
+        default: cn('h-10', 'px-4', 'py-2'),
+        sm: cn('rounded-md', 'px-3', 'py-2'),
+        lg: cn('h-11', 'rounded-md', 'px-8'),
+        icon: cn('h-10', 'w-10'),
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 'default',
+      size: 'default',
     },
   },
 );
@@ -65,12 +65,12 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+    const Comp = asChild ? Slot : 'button';
     return (
       <Comp className={cn(buttonVariants({ variant, size }), className)} ref={ref} {...props} />
     );
   },
 );
-Button.displayName = "Button";
+Button.displayName = 'Button';
 
 export { Button, buttonVariants };
