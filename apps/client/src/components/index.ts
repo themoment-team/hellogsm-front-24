b4743398c212b8987514e194bdb02ffc2fd9673c
예빈 <yebin060310@gmail.com>
2024-08-09 02:34:05 +0900

    Add section2

--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -3,4 +3,5 @@ export * from './ui';
 export { default as ActiveLink } from './ActiveLink';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
+export { default as Section2 } from './MainPage/Section2';
 export { default as TestCalculatePage } from './TestCalculatePage';
