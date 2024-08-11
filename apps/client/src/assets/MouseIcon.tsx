e98d4a9a638e9ccf3bf9594a910315f204e9328a
gjaegyun <jaegyungim06@gmail.com>
2024-08-12 08:23:25 +0900

    refactor: px -> rem

--- apps/client/src/assets/MouseIcon.tsx
+++ apps/client/src/assets/MouseIcon.tsx
@@ -1,5 +1,11 @@
 const MouseIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1rem"
+    height="1.0625rem"
+    viewBox="0 0 16 17"
+    fill="none"
+  >
     <path
       d="M12 6.03906C12 3.82992 10.2091 2.03906 8 2.03906C5.79086 2.03906 4 3.82992 4 6.03906V10.0391C4 12.2482 5.79086 14.0391 8 14.0391C10.2091 14.0391 12 12.2482 12 10.0391V6.03906Z"
       stroke="#94A3B8"
