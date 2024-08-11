4fc9bf208fa44da75ad151c54832547cd70f7aa8
gjaegyun <jaegyungim06@gmail.com>
2024-08-11 23:31:00 +0900

    feat: confirmBar component 제작

--- apps/client/src/assets/index.ts
+++ apps/client/src/assets/index.ts
@@ -4,3 +4,4 @@ export { default as GreenDot } from './GreenDot';
 export { default as HeaderProfileIcon } from './HeaderProfileIcon';
 export { default as SelectIcon } from './SelectIcon';
 export { default as SquareIcon } from './SquareIcon';
+export { default as MouseIcon } from './MouseIcon';
---
+++ apps/client/src/components/ConfirmBar/index.tsx
@@ -0,0 +1,58 @@
+import { MouseIcon } from 'client/assets';
+import { Button } from 'shared';
+import { cn } from 'shared/lib/utils';
+
+const ConfirmBar = () => {
+  const SubmitButton = () => {
+    return (
+      <button
+        className={cn(
+          'inline-flex',
+          'items-center',
+          'justify-center',
+          'rounded-md',
+          'px-4',
+          'py-2',
+          'gap-[0.5rem]',
+          'bg-[#F1F5F9]',
+          'h-10',
+        )}
+      >
+        <div className={cn('w-[1rem]', 'h-[1rem]')}>
+          <MouseIcon />
+        </div>
+        <p className={cn('text-sm', 'font-medium', 'text-[#94A3B8]')}>원서 최종</p>
+      </button>
+    );
+  };
+
+  return (
+    <div
+      className={cn(
+        'w-full',
+        'h-[5rem]',
+        'bg-white',
+        'border-t-solid',
+        'border-t-2',
+        'px-[20rem]',
+        'flex',
+        'justify-between',
+        'items-center',
+      )}
+    >
+      <div>
+        <span className={cn('text-body1', 'text-[#2563EB]')}>
+          📎 최종 제출 후에는 정보를 수정할 수 없습니다. &nbsp;
+        </span>
+        <span className={cn('text-body1', 'text-[#0F172A]')}>정확히 입력 후 제출해주세요!</span>
+      </div>
+
+      <div className={cn('flex', 'items-center', 'gap-[0.5rem]')}>
+        <Button variant="outline" children="임시 저장" />
+        <SubmitButton />
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmBar;
