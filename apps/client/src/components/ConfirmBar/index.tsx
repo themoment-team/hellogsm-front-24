206f1e9e6df98e3be3fefbf4f582c0e135553568
gjaegyun <jaegyungim06@gmail.com>
2024-08-12 08:53:52 +0900

    feat: submit 버튼 생성

--- apps/client/src/components/ConfirmBar/index.tsx
+++ apps/client/src/components/ConfirmBar/index.tsx
@@ -7,29 +7,6 @@ import { MouseIcon } from 'client/assets';
 import { cn } from 'shared/lib/utils';
 
 const ConfirmBar = () => {
-  const SubmitButton = () => {
-    return (
-      <button
-        className={cn(
-          'inline-flex',
-          'items-center',
-          'justify-center',
-          'rounded-md',
-          'px-4',
-          'py-2',
-          'gap-[0.5rem]',
-          'bg-[#F1F5F9]',
-          'h-10',
-        )}
-      >
-        <div className={cn('w-[1rem]', 'h-[1rem]')}>
-          <MouseIcon />
-        </div>
-        <p className={cn('text-sm', 'font-medium', 'text-[#94A3B8]')}>원서 최종</p>
-      </button>
-    );
-  };
-
   return (
     <div
       className={cn(
@@ -53,7 +30,10 @@ const ConfirmBar = () => {
 
       <div className={cn('flex', 'items-center', 'gap-[0.5rem]')}>
         <Button variant="outline">임시저장</Button>
-        <SubmitButton />
+        <Button variant="submit">
+          <MouseIcon />
+          <p>원서 최종</p>
+        </Button>
       </div>
     </div>
   );
