fbfc757e57d62faba562bf0af2b9c44ead23838e
gjaegyun <jaegyungim06@gmail.com>
2024-08-12 09:08:34 +0900

    refactor: 불필요한 cn 제거

--- apps/client/src/assets/MouseIcon.tsx
+++ apps/client/src/assets/MouseIcon.tsx
@@ -1,8 +1,5 @@
-import { cn } from 'shared/lib/utils';
-
 const MouseIcon = () => (
   <svg
-    className={cn('w-[1rem]', 'h-[1rem]')}
     xmlns="http://www.w3.org/2000/svg"
     width="1rem"
     height="1.0625rem"
