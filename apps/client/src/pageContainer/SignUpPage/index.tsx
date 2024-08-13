a84545774c6c6be0728b6128ba4ec80dd3c078b1
Lee-Seungje <moondgod@gmail.com>
2024-08-13 14:10:05 +0900

    refactor: 각 step들 useReducer로 관리

--- apps/client/src/pageContainer/SignUpPage/index.tsx
+++ apps/client/src/pageContainer/SignUpPage/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useReducer } from 'react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -27,6 +27,7 @@ import {
 import { cn } from 'shared/lib/utils';
 
 const PERMIT_YEAR = 50; // 50년 전까지의 연도만 허용함.
+// const phoneNumberRegexp = /^\d{10,11}$/;
 
 interface SexToggleProps extends React.HTMLAttributes<HTMLDivElement> {
   isSelected: boolean;
@@ -56,18 +57,50 @@ const SexToggle = ({ children, isSelected, ...props }: SexToggleProps) => {
   );
 };
 
-const phoneNumberRegexp = /^\d{10,11}$/;
+const initialState = {
+  phoneNumber: '',
+  isCertificationButtonDisabled: true,
+  isSubmitButtonDisabled: true,
+  isSentCertificationNumber: false,
+  certificationNumber: '',
+  isValidCertificationNumber: false,
+  isAgreed: false,
+};
+
+type State = typeof initialState;
+
+type Action =
+  | { type: 'UPDATE_PHONE_NUMBER'; payload: string }
+  | { type: 'TOGGLE_CERTIFICATION_BUTTON'; payload: boolean }
+  | { type: 'SET_CERTIFICATION_SENT'; payload: boolean }
+  | { type: 'UPDATE_CERTIFICATION_NUMBER'; payload: string }
+  | { type: 'SET_CERTIFICATION_VALID'; payload: boolean }
+  | { type: 'TOGGLE_AGREEMENT'; payload: boolean }
+  | { type: 'UPDATE_SUBMIT_BUTTON_STATE'; payload: boolean };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'UPDATE_PHONE_NUMBER':
+      return { ...state, phoneNumber: action.payload };
+    case 'TOGGLE_CERTIFICATION_BUTTON':
+      return { ...state, isCertificationButtonDisabled: action.payload };
+    case 'SET_CERTIFICATION_SENT':
+      return { ...state, isSentCertificationNumber: action.payload };
+    case 'UPDATE_CERTIFICATION_NUMBER':
+      return { ...state, certificationNumber: action.payload };
+    case 'SET_CERTIFICATION_VALID':
+      return { ...state, isValidCertificationNumber: action.payload };
+    case 'TOGGLE_AGREEMENT':
+      return { ...state, isAgreed: action.payload };
+    case 'UPDATE_SUBMIT_BUTTON_STATE':
+      return { ...state, isSubmitButtonDisabled: !action.payload };
+    default:
+      return state;
+  }
+};
 
 const SignUpPage = () => {
-  const flexColStyle = ['flex', 'flex-col'] as const;
-
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
-  const [disableCertificationButton, setDisableCertificationButton] = useState<boolean>(true);
-  const [disableSubmitButton, setDisableSubmitButton] = useState<boolean>(true);
-  const [isSentCertificationNumber, setIsSentCertificationNumber] = useState<boolean>(false);
-  const [certificationNumber, setCertificationNumber] = useState<string>('');
-  const [isValidCertificationNumber, setIsValidCertificationNumber] = useState<boolean>(false);
-  const [isAgreed, setIsAgreed] = useState<boolean>(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const form = useForm<z.infer<typeof signupFormSchema>>({
     resolver: zodResolver(signupFormSchema),
@@ -90,39 +123,39 @@ const SignUpPage = () => {
 
   const targetYear = new Date().getFullYear() - PERMIT_YEAR;
 
-  const handelPhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const currentValue = e.target.value;
-    setPhoneNumber(currentValue);
+    dispatch({ type: 'UPDATE_PHONE_NUMBER', payload: currentValue });
 
-    setDisableCertificationButton(!phoneNumberRegexp.test(currentValue));
+    const isPhoneNumberValid = /^\d{10,11}$/.test(currentValue);
+    dispatch({ type: 'TOGGLE_CERTIFICATION_BUTTON', payload: !isPhoneNumberValid });
   };
 
   const handleCertificationButtonClick = () => {
-    setIsSentCertificationNumber(true);
-  };
-
-  const checkCanSubmit = (isValidCertificationNumber: boolean, isAgreed: boolean) => {
-    setDisableSubmitButton(!(isValidCertificationNumber && isAgreed));
+    dispatch({ type: 'SET_CERTIFICATION_SENT', payload: true });
   };
 
   const handleCertificationNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const currentValue = e.target.value;
-    setCertificationNumber(currentValue);
+    dispatch({ type: 'UPDATE_CERTIFICATION_NUMBER', payload: currentValue });
 
     const isValid = currentValue === '서버에서 보내준 인증번호';
-    setIsValidCertificationNumber(isValid);
-    checkCanSubmit(isValid, isAgreed);
+    dispatch({ type: 'SET_CERTIFICATION_VALID', payload: isValid });
+    dispatch({ type: 'UPDATE_SUBMIT_BUTTON_STATE', payload: isValid && state.isAgreed });
   };
 
   const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
     const currentValue = e.target.checked;
-    setIsAgreed(currentValue);
-    checkCanSubmit(isValidCertificationNumber, currentValue);
+    dispatch({ type: 'TOGGLE_AGREEMENT', payload: currentValue });
+    dispatch({
+      type: 'UPDATE_SUBMIT_BUTTON_STATE',
+      payload: state.isValidCertificationNumber && currentValue,
+    });
   };
 
   return (
-    <main className={cn(...flexColStyle, 'items-center', 'gap-10')}>
-      <div className={cn(...flexColStyle, 'gap-3', 'items-center')}>
+    <main className={cn('flex', 'flex-col', 'items-center', 'gap-10', 'pb-40', 'pt-[3.75rem]')}>
+      <div className={cn('flex', 'flex-col', 'gap-3', 'items-center')}>
         <h1 className={cn('text-2xl', 'font-semibold')}>회원가입</h1>
         <p className={cn('text-sm', 'font-normal', 'text-gray-600')}>
           가입 이후 <span className={cn('font-semibold')}>정보 수정이 불가</span>하니 정보를 정확히
@@ -131,7 +164,7 @@ const SignUpPage = () => {
       </div>
 
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className={cn(...flexColStyle, 'gap-4')}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className={cn('flex', 'flex-col', 'gap-4')}>
           <FormField
             control={control}
             name="name"
@@ -171,11 +204,11 @@ const SignUpPage = () => {
                 render={({ field }) => (
                   <FormItem>
                     <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value ? field.value.year : ''}
+                      onValueChange={(value) => setValue('birth.year', value)}
+                      defaultValue={field.value?.year ?? ''}
                     >
                       <FormControl>
-                        <SelectTrigger className="w-[121px]">
+                        <SelectTrigger className="w-[7.5625rem]">
                           <SelectValue placeholder="년도" />
                         </SelectTrigger>
                       </FormControl>
@@ -202,11 +235,11 @@ const SignUpPage = () => {
                 render={({ field }) => (
                   <FormItem>
                     <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value ? field.value.month : ''}
+                      onValueChange={(value) => setValue('birth.month', value)}
+                      defaultValue={field.value?.month ?? ''}
                     >
                       <FormControl>
-                        <SelectTrigger className="w-[121px]">
+                        <SelectTrigger className="w-[7.5625rem]">
                           <SelectValue placeholder="월" />
                         </SelectTrigger>
                       </FormControl>
@@ -230,11 +263,11 @@ const SignUpPage = () => {
                 render={({ field }) => (
                   <FormItem>
                     <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value ? field.value.day : ''}
+                      onValueChange={(value) => setValue('birth.day', value)}
+                      defaultValue={field.value?.day ?? ''}
                     >
                       <FormControl>
-                        <SelectTrigger className="w-[121px]">
+                        <SelectTrigger className="w-[7.5625rem]">
                           <SelectValue placeholder="일" />
                         </SelectTrigger>
                       </FormControl>
@@ -255,27 +288,27 @@ const SignUpPage = () => {
             </div>
           </CustomFormItem>
           <CustomFormItem gap="small" text="전화번호">
-            <div className={cn(...flexColStyle, 'gap-1.5')}>
+            <div className={cn('flex', 'flex-col', 'gap-1.5')}>
               <div className={cn('flex', 'justify-between')}>
-                <div className="w-[288px]">
+                <div className="w-[18rem]">
                   <Input
-                    value={phoneNumber}
-                    onChange={handelPhoneNumberChange}
+                    value={state.phoneNumber}
+                    onChange={handlePhoneNumberChange}
                     placeholder="번호 입력"
                   />
                 </div>
                 <Button
                   type="button"
                   variant="disabled"
-                  disabled={disableCertificationButton}
+                  disabled={state.isCertificationButtonDisabled}
                   onClick={handleCertificationButtonClick}
                 >
                   번호 인증
                 </Button>
               </div>
               <Input
-                disabled={!isSentCertificationNumber}
-                value={certificationNumber}
+                disabled={!state.isSentCertificationNumber}
+                value={state.certificationNumber}
                 onChange={handleCertificationNumberChange}
                 placeholder="인증번호 6자리 입력"
               />
@@ -284,7 +317,7 @@ const SignUpPage = () => {
 
           <input type="checkbox" onChange={handleCheck} />
 
-          <Button variant="disabled" disabled={disableSubmitButton}>
+          <Button type="submit" variant="disabled" disabled={state.isSubmitButtonDisabled}>
             회원가입 완료
           </Button>
         </form>
