de80c5d75aa4aa52fd55dd69a77eec61e55722c3
gjaegyun <jaegyungim06@gmail.com>
2024-08-14 23:52:33 +0900

    feat: upload photo 컴포넌트 제작

---
+++ apps/client/src/components/UploadPhoto/index.stories.tsx
@@ -0,0 +1,20 @@
+import UploadPhoto from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof UploadPhoto> = {
+  title: 'Client/UploadPhoto',
+  component: UploadPhoto,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof UploadPhoto>;
+
+export const Primary: Story = {};
+
+export const Required: Story = {
+  args: {
+    required: true,
+  },
+};
---
+++ apps/client/src/components/UploadPhoto/index.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { useState } from 'react';
+
+import Image from 'next/image';
+
+import { UploadIcon } from 'client/assets';
+
+import { cn } from 'shared/lib/utils';
+
+interface UploadPhotoProps {
+  required?: boolean;
+}
+
+const UploadPhoto = ({ required }: UploadPhotoProps) => {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  const PhotoUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          setImageSrc(reader.result);
+        }
+      };
+    }
+  };
+
+  return (
+    <div className={cn('flex', 'items-end', 'gap-[0.5rem]')}>
+      <div className={cn('flex', 'w-[8.75rem]', 'flex-col', 'items-start', 'gap-[0.25rem]')}>
+        <p className={cn('text-slate-900', 'text-body2', 'font-medium')}>
+          증명사진 {required && <span className="text-red-600">*</span>}
+        </p>
+
+        <input
+          className={cn('hidden')}
+          type="file"
+          id="file-input"
+          multiple
+          accept=".jpg, .jpeg, .png"
+          onChange={PhotoUpload}
+        />
+
+        <label
+          className={cn(
+            'flex',
+            'w-[8.75rem]',
+            'h-[10rem]',
+            'bg-[#F5F6F8]',
+            'border-2',
+            'rounded-lg',
+            'border-gray-200',
+            'justify-center',
+            'items-center',
+            'items-center',
+            'gap-[0.625rem]',
+            'cursor-pointer',
+          )}
+          htmlFor="file-input"
+        >
+          {imageSrc ? <Image src={imageSrc} alt="Uploaded" /> : <UploadIcon />}
+        </label>
+      </div>
+      <ul className={cn('text-slate-600', 'text-caption', 'font-[400]')}>
+        <li>&middot; 20MB 이하</li>
+        <li>&middot; 3개월 이내의 3x4 cm 증명사진</li>
+      </ul>
+    </div>
+  );
+};
+
+export default UploadPhoto;
