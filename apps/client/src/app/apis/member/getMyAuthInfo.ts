1762c82121cf099b83d884f05b9321e128baef99
Lee-Seungje <moondgod@gmail.com>
2024-08-14 15:18:03 +0900

    feat: MyAuthInfoType 추가

--- apps/client/src/app/apis/member/getMyAuthInfo.ts
+++ apps/client/src/app/apis/member/getMyAuthInfo.ts
@@ -2,14 +2,14 @@ import { redirect } from 'next/navigation';
 
 import { memberUrl } from 'api/libs';
 
-import type { MyMemberInfoType } from 'types';
+import type { MyAuthInfoType } from 'types';
 
 /**
  * 나의 인증 정보를 가져옵니다.
  *
  * @returns 나의 인증 정보를 반환합니다. 없다면 -> TODO 서버 문서 업데이트 중입니다.
  */
-export const getMyAuthInfo = async (redirectUrl: string): Promise<MyMemberInfoType | undefined> => {
+export const getMyAuthInfo = async (redirectUrl: string): Promise<MyAuthInfoType | undefined> => {
   const response = await fetch(new URL(`/api${memberUrl.getMyAuthInfo()}`, process.env.BASE_URL), {
     method: 'GET',
   });
--- packages/api/src/hooks/api/member/useGetMyAuthInfo.ts
+++ packages/api/src/hooks/api/member/useGetMyAuthInfo.ts
@@ -1,18 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { memberUrl, memberQueryKeys, get } from 'api/libs';
-import { MyMemberInfoType } from 'types';
+import { MyAuthInfoType } from 'types';
 import { minutesToMs } from 'shared/utils';
 
 import type { UseQueryOptions } from '@tanstack/react-query';
 
 export const useGetMyAuthInfo = (
-  initialData?: MyMemberInfoType | undefined,
-  options?: Omit<UseQueryOptions<MyMemberInfoType | undefined>, 'queryKey'>,
+  initialData?: MyAuthInfoType | undefined,
+  options?: Omit<UseQueryOptions<MyAuthInfoType | undefined>, 'queryKey'>,
 ) =>
   useQuery({
     queryKey: memberQueryKeys.getMyAuthInfo(),
-    queryFn: () => get<MyMemberInfoType>(memberUrl.getMyAuthInfo()),
+    queryFn: () => get<MyAuthInfoType>(memberUrl.getMyAuthInfo()),
     staleTime: minutesToMs(5),
     gcTime: minutesToMs(5),
     ...options,
