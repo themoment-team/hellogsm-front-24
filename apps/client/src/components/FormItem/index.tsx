cdcd53790421bdcde905f0e6590fe31c55b6569e
gjaegyun <jaegyungim06@gmail.com>
2024-08-15 02:45:39 +0900

    feat: full width 추가

--- apps/client/src/components/FormItem/index.tsx
+++ apps/client/src/components/FormItem/index.tsx
@@ -8,11 +8,20 @@ interface FormItemProps extends PropsWithChildren {
   text: string;
   required?: boolean;
   className?: string;
+  fullWidth?: boolean;
 }
 
-const FormItem = ({ children, text, className, required }: FormItemProps) => {
+const FormItem = ({ children, text, className, required, fullWidth }: FormItemProps) => {
   return (
-    <div className={cn(className, 'flex', 'flex-col', 'w-[23.75rem]')}>
+    <div
+      className={cn(
+        className,
+        'flex',
+        'flex-col',
+        'w-[23.75rem]',
+        fullWidth ? 'w-full' : 'w-[23.75rem]',
+      )}
+    >
       <span className={cn('text-gray-900', 'text-sm', 'font-medium')}>
         {text} {required && <span className="text-red-600">*</span>}
       </span>
