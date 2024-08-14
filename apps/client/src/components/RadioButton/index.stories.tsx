94c7e1fa20a5640f198c534a15ec2b267a887647
gjaegyun <jaegyungim06@gmail.com>
2024-08-14 16:11:07 +0900

    feat: radio button 컴포넌트 추가

---
+++ apps/client/src/components/RadioButton/index.stories.tsx
@@ -0,0 +1,28 @@
+import RadioButton from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof RadioButton> = {
+  title: 'Client/RadioButton',
+  component: RadioButton,
+  args: {
+    title: '성별',
+    options: ['남자', '여자'],
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RadioButton>;
+
+export const Primary: Story = {
+  args: {
+    required: false,
+  },
+};
+
+export const Required: Story = {
+  args: {
+    required: true,
+  },
+};
---
+++ apps/client/src/components/RadioButton/index.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { cn } from 'shared/lib/utils';
+
+interface RadioButtonProps {
+  title: string;
+  options: string[];
+  required: boolean;
+}
+
+const RadioButton = ({ title, options, required }: RadioButtonProps) => {
+  const textStyle = ['text-slate-900', 'text-body2', 'font-medium'];
+  return (
+    <div className={cn('w-full', 'flex', 'flex-col', 'items-start', 'gap-[0.75rem]')}>
+      <div className={cn(...textStyle)}>
+        {title} {required && <span className="text-red-600">*</span>}
+      </div>
+      <div className={cn('flex', 'items-start', 'gap-[1.25rem]')}>
+        {options.map((option, index) => (
+          <div key={index} className={cn('flex', 'items-center', 'gap-[0.5rem]')}>
+            <input
+              type="radio"
+              name="radioBtn"
+              value={option}
+              className={cn(
+                'appearance-none',
+                'w-4',
+                'h-4',
+                'border-[1px]',
+                'border-slate-200',
+                'rounded-full',
+                'relative',
+                'after:content-[""]',
+                'after:absolute',
+                'after:top-1/2',
+                'after:left-1/2',
+                'after:w-2',
+                'after:h-2',
+                'after:rounded-full',
+                'after:bg-black',
+                'after:transform',
+                'after:-translate-x-1/2',
+                'after:-translate-y-1/2',
+                'checked:after:scale-100',
+                'after:scale-0',
+                'transition-transform',
+              )}
+            />
+            <p className={cn(...textStyle)}>{option}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RadioButton;
