b75c2947f64af368589a58ecbc69e50507ea0be5
gaoooon <gaon9749@gmail.com>
2024-08-16 06:06:35 +0900

    Add Icons

---
+++ apps/client/src/assets/PinOffIcon.tsx
@@ -0,0 +1,47 @@
+const PinOffIcon = () => (
+  <svg
+    width="0.9375rem"
+    height="0.875rem"
+    viewBox="0 0 15 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_1924_5863)">
+      <path
+        d="M1.3667 1.16602L13.0334 12.8327"
+        stroke="#94A3B8"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.2002 9.91602V12.8327"
+        stroke="#94A3B8"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.45003 5.25V6.27667C5.44992 6.49372 5.38926 6.70643 5.27487 6.89089C5.16048 7.07535 4.99691 7.22425 4.80253 7.32083L3.7642 7.84583C3.56983 7.94242 3.40625 8.09132 3.29186 8.27578C3.17748 8.46024 3.11681 8.67295 3.1167 8.89V9.91667H10.1167"
+        stroke="#94A3B8"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.95023 5.44768V3.49935H9.53357C9.84299 3.49935 10.1397 3.37643 10.3585 3.15764C10.5773 2.93885 10.7002 2.6421 10.7002 2.33268C10.7002 2.02326 10.5773 1.72652 10.3585 1.50772C10.1397 1.28893 9.84299 1.16602 9.53357 1.16602H4.80273"
+        stroke="#94A3B8"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_1924_5863">
+        <rect width="14" height="14" fill="white" transform="translate(0.200195)" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default PinOffIcon;
