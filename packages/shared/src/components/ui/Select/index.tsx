3e8566e4a9cd2e55746801d8f9515d2559ead4e2
gaoooon <gaon9749@gmail.com>
2024-08-16 18:24:27 +0900

    Update Select

--- packages/shared/src/components/ui/Select/index.tsx
+++ packages/shared/src/components/ui/Select/index.tsx
@@ -47,7 +47,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className="w-4 h-4 opacity-50" />
+      <ChevronDown className="h-4 w-4 stroke-[#71717A]" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
@@ -62,7 +62,7 @@ const SelectScrollUpButton = React.forwardRef<
     className={cn('flex', 'cursor-default', 'items-center', 'justify-center', 'py-1', className)}
     {...props}
   >
-    <ChevronUp className="w-4 h-4" />
+    <ChevronUp className="h-4 w-4" />
   </SelectPrimitive.ScrollUpButton>
 ));
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName;
@@ -76,7 +76,7 @@ const SelectScrollDownButton = React.forwardRef<
     className={cn('flex', 'cursor-default', 'items-center', 'justify-center', 'py-1', className)}
     {...props}
   >
-    <ChevronDown className="w-4 h-4" />
+    <ChevronDown className="h-4 w-4" />
   </SelectPrimitive.ScrollDownButton>
 ));
 SelectScrollDownButton.displayName = SelectPrimitive.ScrollDownButton.displayName;
@@ -170,13 +170,14 @@ const SelectItem = React.forwardRef<
       'focus:text-accent-foreground',
       'data-[disabled]:pointer-events-none',
       'data-[disabled]:opacity-50',
+      'cursor-pointer',
       className,
     )}
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <Check className="w-4 h-4" />
+        <Check className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
     </span>
 
