d94755714091f2f1b6bda5eb2ea4afc49ecda4d4
Lee-Seungje <moondgod@gmail.com>
2024-08-22 01:24:39 +0900

    feat: title 제작

--- apps/client/src/components/MainPage/Section1/index.tsx
+++ apps/client/src/components/MainPage/Section1/index.tsx
@@ -6,10 +6,55 @@ import Video from './Video';
 
 const Section1 = () => (
   <div
-    className={cn('w-full', 'h-dvh', 'fixed', 'top-0', 'z-0', 'flex', 'justify-center', 'bg-black')}
+    className={cn(
+      'w-full',
+      'h-dvh',
+      'relative',
+      'top-0',
+      'z-0',
+      'flex',
+      'justify-center',
+      'bg-black',
+    )}
   >
     <Video />
-    <div className={cn('w-full', 'h-dvh', 'bg-[rbga(0,0,0,0.5)]', 'absolute', 'top-0', 'z-[2]')} />
+    <div
+      className={cn(
+        'w-full',
+        'h-dvh',
+        'bg-[rgba(0, 0, 0, 0.40)]',
+        'absolute',
+        'top-0',
+        'z-[2]',
+        'flex',
+        'items-center',
+        'justify-center',
+        'opacity-80',
+      )}
+    >
+      <h1
+        className={cn('w-[49.0625rem]', 'text-center', 'text-[3.25rem]', 'font-bold', 'text-white')}
+      >
+        꿈🌟과 끼🤘🏻를 마음껏{' '}
+        <span
+          className={cn(
+            'relative',
+            'before:absolute',
+            'before:-top-[2.625rem]',
+            'before:left-2',
+            'before:text-lime-400',
+            'before:tracking-[0.8125rem]',
+            'text-sky-300',
+            'before:text-[2.5rem]',
+            // eslint-disable-next-line quotes
+            "before:content-['••']",
+          )}
+        >
+          UP
+        </span>{' '}
+        시킬 수 있는 광주소프트웨어마이스터고등학교
+      </h1>
+    </div>
   </div>
 );
 
