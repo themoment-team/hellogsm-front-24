829514588d7a132aa6d8142ffdf0e1c94c481295
gaoooon <gaon9749@gmail.com>
2024-08-24 11:10:00 +0900

    Update getMyOneseo

--- apps/client/src/app/apis/oneseo/getMyOneseo.ts
+++ apps/client/src/app/apis/oneseo/getMyOneseo.ts
@@ -1,17 +1,25 @@
+import { cookies } from 'next/headers';
 import { GetMyOneseoType } from 'types';
 
 import { oneseoUrl } from 'api/libs';
 
 export const getMyOneseo = async (): Promise<GetMyOneseoType | undefined> => {
+  const session = cookies().get('SESSION')?.value;
+
   const response = await fetch(
-    new URL(`/api${oneseoUrl.getMyOneseo()}}`, process.env.NEXT_PUBLIC_API_BASE_URL),
+    new URL(`${oneseoUrl.getMyOneseo()}}`, process.env.NEXT_PUBLIC_API_BASE_URL),
     {
       method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: `SESSION=${session}`,
+      },
     },
   );
-  const myOneseo: GetMyOneseoType = await response.json();
+  const myOneseo = await response.json();
 
   if (response.status === 404) return undefined;
 
-  return myOneseo;
+  return myOneseo.data;
 };
