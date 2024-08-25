220f013ea304e7b509837c95deec027bb1719abf
Lee-Seungje <moondgod@gmail.com>
2024-08-25 22:22:18 +0900

    fix: date 적용, 스타일 수정

--- apps/admin/src/pageContainer/TicketPage/index.tsx
+++ apps/admin/src/pageContainer/TicketPage/index.tsx
@@ -5,6 +5,15 @@ import { TicketType } from 'types';
 
 import { TicketsPrintIcon } from 'admin/assets';
 
+import { Button } from 'shared/components';
+import {
+  visionCampDate,
+  passedMemberAnnounceDate,
+  passedMemberSubmitDate,
+  심층면접시험기간,
+  직무적성시험기간,
+} from 'shared/constants';
+
 import { useGetAdmissionTickets } from 'api/hooks';
 
 interface TicketPageProps {
@@ -21,34 +30,38 @@ const TicketPage = ({ initialData }: TicketPageProps) => {
   };
 
   return (
-    <div className="print:exact bg-white pt-[4.16vh] print:pt-0">
+    <div className="print:exact flex flex-col gap-4 bg-white pt-[4.16vh] print:pt-0">
       {admissionTickets?.map((ticket, i) => (
         <div
           key={ticket.oneseoSubmitCode}
-          className={`mx-auto mb-[26.97vh] h-[31.64vh] w-[63vh] ${
+          className={`mx-auto h-[31.64vh] w-[920px] font-normal text-black ${
             i % 2 !== 0 ? 'mb-[27.14vh] mt-[8.32vh]' : ''
           }`}
         >
           <table className="h-full w-full border-collapse text-center text-[0.75rem] font-light leading-[1.25rem]">
             <tbody>
               <tr>
-                <td className="w-[55%]" colSpan={2}>
+                <td className="w-[55vw] border border-black text-[20px] font-normal" colSpan={2}>
                   수험자 안내사항
                 </td>
-                <td className="w-[45%]" colSpan={3}>
+                <td className="w-[45vw] border border-black text-[20px] font-normal" colSpan={3}>
                   수 험 표
                 </td>
               </tr>
               <tr>
-                <td className="border border-black">2차 전형 (직무적성소양평가)</td>
-                <td className="border border-black pl-[0.625rem] text-left">
-                  2023. 10. 27.(금) 13:00 ~ 16:10
+                <td className="font-normal border border-black w-28">
+                  2-1차 전형 <br />
+                  (직무적성소양평가)
                 </td>
-                <td rowSpan={6} className="w-[20%] border border-black">
+                <td className="border border-black pl-[0.625rem] text-left font-normal">
+                  {직무적성시험기간}
+                </td>
+
+                <td rowSpan={6} className="h-[200px] w-[150px] border border-black">
                   {ticket.profileImg && (
                     <img
                       src={
-                        // 현재 프로필 이미지가 정상적이지 않아 예시 이미지를 넣었습니다. TODO 제거
+                        // TODO 제거. 현재 프로필 이미지가 정상적이지 않아 예시 이미지를 넣었습니다.
                         ticket.profileImg === 'https://asd'
                           ? 'https://cdn.hankyung.com/photo/201904/01.19372617.1.jpg'
                           : ticket.profileImg
@@ -58,41 +71,66 @@ const TicketPage = ({ initialData }: TicketPageProps) => {
                     />
                   )}
                 </td>
-                <td className="w-[10%] border border-black bg-[#e9e9e9] font-medium">접수번호</td>
-                <td className="border border-black">{ticket.oneseoSubmitCode}</td>
+                <td
+                  rowSpan={2}
+                  className="w-[10%] border border-black bg-[#e9e9e9] font-medium font-normal"
+                >
+                  접수번호
+                </td>
+                <td rowSpan={2} className="font-normal border border-black">
+                  {ticket.oneseoSubmitCode}
+                </td>
               </tr>
               <tr>
-                <td rowSpan={2} className="border border-black">
+                <td className="font-normal border border-black w-28">
+                  2-2차 전형 <br />
+                  (심층면접)
+                </td>
+                <td className="border border-black pl-[0.625rem] text-left font-normal">
+                  {심층면접시험기간}
+                </td>
+              </tr>
+              <tr>
+                <td rowSpan={2} className="font-normal border border-black">
                   합격자 발표
                 </td>
-                <td className="border border-black pl-[0.625rem] text-left">본교 홈페이지</td>
-                <td className="border border-black bg-[#e9e9e9] font-medium">성명</td>
+                <td className="border border-black pl-[0.625rem] text-left font-normal">
+                  본교 홈페이지
+                </td>
+                <td className="border border-black bg-[#e9e9e9] font-medium font-normal">성명</td>
                 <td className="border border-black">{ticket.memberName}</td>
               </tr>
               <tr>
-                <td className="border border-black pl-[0.625rem] text-left">
-                  2023. 11. 1.(수) 10:00
+                <td className="border border-black pl-[0.625rem] text-left font-normal">
+                  {passedMemberAnnounceDate}
                 </td>
-                <td className="border border-black bg-[#e9e9e9] font-medium" rowSpan={2}>
+                <td
+                  className="border border-black bg-[#e9e9e9] font-medium font-normal"
+                  rowSpan={2}
+                >
                   생년월일
                 </td>
-                <td className="border border-black" rowSpan={2}>
+                <td className="font-normal border border-black" rowSpan={2}>
                   {ticket.memberBirth}
                 </td>
               </tr>
               <tr>
-                <td rowSpan={2} className="border border-black">
-                  합격자 등록 및 서류 제출
+                <td rowSpan={2} className="font-normal border border-black">
+                  합격자 등록 및<br /> 서류 제출
                 </td>
-                <td className="border border-black pl-[0.625rem] text-left">공문 및 방문 제출</td>
+                <td className="border border-black pl-[0.625rem] text-left font-normal">
+                  공문 및 방문 제출
+                </td>
               </tr>
               <tr>
-                <td className="border border-black pl-[0.625rem] text-left">
-                  2023. 11. 1.(수) ~ 2023. 11. 6.(월) <br />
+                <td className="border border-black pl-[0.625rem] text-left font-normal">
+                  {passedMemberSubmitDate.startDate} ~ {passedMemberSubmitDate.endDate} <br />
                   09:00 ~ 17:00 (토·일요일 제외, <br />
                   마감시간 이전 도착분에 한하여 유효함.)
                 </td>
-                <td className="border border-black bg-[#e9e9e9] font-medium">출신학교</td>
+                <td className="border border-black bg-[#e9e9e9] font-medium font-normal">
+                  출신학교
+                </td>
                 {ticket.schoolName ? (
                   <td className="border border-black">{ticket.schoolName}</td>
                 ) : (
@@ -100,46 +138,50 @@ const TicketPage = ({ initialData }: TicketPageProps) => {
                 )}
               </tr>
               <tr>
-                <td className="border border-black">합격자 서류</td>
-                <td className="border border-black pl-[0.625rem] text-left">공고문 참조</td>
-                <td className="border border-black bg-[#e9e9e9] font-medium">전형구분</td>
-                <td className="border border-black">{ScreeningEnum[ticket.appliedScreening]}</td>
+                <td className="font-normal border border-black">합격자 서류</td>
+                <td className="border border-black pl-[0.625rem] text-left font-normal">
+                  공고문 참조
+                </td>
+                <td
+                  colSpan={2}
+                  className="border border-black bg-[#e9e9e9] font-medium font-normal"
+                >
+                  전형구분
+                </td>
+                <td className="font-normal border border-black">
+                  {ScreeningEnum[ticket.appliedScreening]}
+                </td>
               </tr>
               <tr>
-                <td className="border border-black">신입생 비전캠프</td>
-                <td className="border border-black pl-[0.625rem] text-left">
-                  2024. 1. 10.(수) ~ 2024. 1. 12.(금) 예정
+                <td className="font-normal border border-black">신입생 비전캠프</td>
+                <td className="border border-black pl-[0.625rem] text-left font-normal">
+                  {visionCampDate.startDate} ~ {visionCampDate.endDate} 예정
                 </td>
                 <td
                   rowSpan={2}
                   colSpan={2}
-                  className="border border-black pr-[0.625rem] text-right"
+                  className="border border-black pl-2 pr-[0.625rem] text-left font-normal"
                 >
                   위 사람은 2024학년도 <br /> 본교 신입생 입학전형 지원자임을 확인함.
-                  <p className="pr-[0.625rem] text-right">2023년 10월 27일</p>
-                  <p>
-                    광주소프트웨어마이스터고등학교장 <br /> [직인생략]
-                  </p>
+                  <p className="pr-[0.625rem] text-center font-normal">2023년 10월 27일</p>
+                  <p>광주소프트웨어마이스터고등학교장 [직인생략]</p>
                 </td>
-                <td className="border border-black bg-[#e9e9e9] font-medium">접수자</td>
+                <td className="border border-black bg-[#e9e9e9] font-medium font-normal">접수자</td>
               </tr>
               <tr>
-                <td colSpan={2} className="border border-black pl-[0.625rem] text-left">
+                <td colSpan={2} className="border border-black pl-[0.625rem] text-left font-normal">
                   * 공지 사항: 홈페이지(gsm.gen.hs.kr)-입학 안내 <br />* 문의:062-949-6800
                 </td>
-                <td className="border border-black">(인)</td>
+                <td className="font-normal border border-black">(인)</td>
               </tr>
             </tbody>
           </table>
         </div>
       ))}
-      <button
-        onClick={handlePrint}
-        className="fixed bottom-[9vh] right-[9vh] z-10 flex h-[6.5vh] w-[17vh] cursor-pointer items-center justify-between rounded-[0.7vh] border-none bg-white p-[1.8vh] pr-[2.5vh] shadow-[0.3vh_0.3vh_0.5vh_rgba(0,0,0,0.21)] hover:bg-black hover:transition-colors hover:duration-500 print:hidden"
-      >
+      <Button onClick={handlePrint}>
         <TicketsPrintIcon />
         <p className="text-[2.1vh] font-bold hover:text-white">인쇄하기</p>
-      </button>
+      </Button>
     </div>
   );
 };
--- packages/shared/src/constants/date.ts
+++ packages/shared/src/constants/date.ts
@@ -2,3 +2,19 @@ export const RECRUITMENT_PERIOD = {
   startDate: '2024.10.10.',
   endDate: '오전 9시 ~ 오후 4시',
 } as const;
+
+// TODO 정확한 날짜 확인 후 수정
+export const visionCampDate = {
+  startDate: '2025. 01. 10.(수)',
+  endDate: '2025. 01. 12.(금)',
+};
+
+// TODO 정확한 날짜 확인 후 수정
+export const passedMemberAnnounceDate = '2024. 11. 1.(수) 10:00';
+export const passedMemberSubmitDate = {
+  startDate: '2025. 11. 1.(수)',
+  endDate: '2025. 11. 6.(월)',
+};
+
+export const 직무적성시험기간 = '2025. 10. 27.(금) 13:00 ~ 16:10';
+export const 심층면접시험기간 = '2025. 10. 28.(금) 13:00 ~ 16:10';
