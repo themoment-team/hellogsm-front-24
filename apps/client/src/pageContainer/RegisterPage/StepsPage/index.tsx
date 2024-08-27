bb2896a4caa7f123f0d7ac20aa105d37362d84e2
gaoooon <gaon9749@gmail.com>
2024-08-27 17:10:58 +0900

    Add pageLeavePrevent

--- apps/client/src/pageContainer/RegisterPage/StepsPage/index.tsx
+++ apps/client/src/pageContainer/RegisterPage/StepsPage/index.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect } from 'react';
 import { StepsContainer } from 'shared';
 import { GetMyOneseoType } from 'types';
 
@@ -6,8 +9,26 @@ interface RegisterStepsPageProps {
   param: string;
 }
 
-const RegisterStepsPage = ({ data, param }: RegisterStepsPageProps) => (
-  <StepsContainer data={data} param={param} />
-);
+const RegisterStepsPage = ({ data, param }: RegisterStepsPageProps) => {
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      event.preventDefault();
+      event.returnValue = '';
+      return '작성하던 내용이 모두 사라집니다. 계속하시겠습니까?';
+    };
+
+    if (window !== undefined) window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  return (
+    <>
+      <StepsContainer data={data} param={param} />
+    </>
+  );
+};
 
 export default RegisterStepsPage;
