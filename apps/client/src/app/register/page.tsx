29efbb118e32fbd3d943623fbf2fc04f1053a26a
gaoooon <gaon9749@gmail.com>
2024-08-27 17:11:21 +0900

    Update page

--- apps/client/src/app/register/page.tsx
+++ apps/client/src/app/register/page.tsx
@@ -1,5 +1,7 @@
+import { redirect } from 'next/navigation';
+
 import { getMyOneseo } from 'client/app/apis';
-import { RegisterStep4Page, RegisterStepsPage } from 'client/pageContainer';
+import { RegisterStepsPage } from 'client/pageContainer';
 
 interface RegisterProps {
   searchParams?: { [key: string]: string | undefined };
@@ -9,20 +11,7 @@ export default async function Register({ searchParams }: RegisterProps) {
   const step = searchParams?.step;
   const data = await getMyOneseo();
 
-  switch (step) {
-    case '1':
-      return <RegisterStepsPage data={data} param="1" />;
+  if (!step) redirect('/register?step=1');
 
-    case '2':
-      return <RegisterStepsPage data={data} param="2" />;
-
-    case '3':
-      return <RegisterStepsPage data={data} param="3" />;
-
-    case '4':
-      return <RegisterStep4Page data={data} />;
-
-    default:
-      return <div></div>;
-  }
+  return <RegisterStepsPage data={data} param={step} />;
 }
