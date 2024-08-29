d07fbad3535bd232c7685764ba52d0a4a6c49bda
gjaegyun <jaegyungim06@gmail.com>
2024-08-29 21:18:59 +0900

    feat: 비어있는 필드 alertDialog 사용

--- packages/shared/src/components/StepBar/index.tsx
+++ packages/shared/src/components/StepBar/index.tsx
@@ -12,7 +12,15 @@ import {
 import { basicRegisterType, GraduationType, MajorType, ScreeningType } from 'types';
 
 import { StepCheckIcon, ProgressBarIcon } from 'shared/assets';
-import { Button } from 'shared/components';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  Button,
+} from 'shared/components';
 import { cn } from 'shared/lib/utils';
 import { useStore } from 'shared/stores';
 
@@ -67,6 +75,8 @@ const StepBar = ({ param, handleSubmit, watch }: StepBarType) => {
   const store = useStore();
 
   const [currentStep, setCurrentStep] = useState(Steps.ONE);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [errors, setErrors] = useState<string[]>([]);
 
   useEffect(() => {
     const stepNumber = Number(params.get('step')) || Steps.ONE;
@@ -82,15 +92,24 @@ const StepBar = ({ param, handleSubmit, watch }: StepBarType) => {
   };
 
   const handleStep1Errors = () => {
-    const { img, address, detailAddress, phoneNumber } = watch();
-    if (img && address && detailAddress && phoneNumber) {
+    const { img, address, detailAddress } = watch();
+    if (img && address && detailAddress) {
       const { setProfileImg, setAddress, setDetailAddress } = store;
 
       setProfileImg(img);
       setAddress(address);
       setDetailAddress(detailAddress);
+      setErrors([]);
 
       updateStep(Math.min(currentStep + 1, Steps.FOUR));
+    } else {
+      if (errors.length === 0) {
+        if (!img) errors.push('프로필 이미지');
+        if (!address) errors.push('주소');
+        if (!detailAddress) errors.push('상세 주소');
+      }
+
+      setShowModal(true);
     }
   };
 
@@ -132,8 +151,19 @@ const StepBar = ({ param, handleSubmit, watch }: StepBarType) => {
       setGraduationType(categoryConvertor[category]);
       setSchoolName(schoolName);
       setSchoolAddress(schoolAddress);
+      setErrors([]);
 
       updateStep(Math.min(currentStep + 1, Steps.FOUR));
+    } else {
+      if (errors.length === 0) {
+        if (!category) errors.push('지원자 유형');
+        if (!schoolName) errors.push('출신 중학교');
+        if (!year && !month) errors.push('졸업일');
+        if (!screening) errors.push('전형');
+        if (!choice) errors.push('지원학과 선택');
+      }
+
+      setShowModal(true);
     }
   };
 
@@ -166,7 +196,18 @@ const StepBar = ({ param, handleSubmit, watch }: StepBarType) => {
       setSchoolTeacherName(schoolTeacherName);
       setSchoolTeacherPhoneNumber(schoolTeacherPhoneNumber);
 
+      setErrors([]);
       updateStep(Math.min(currentStep + 1, Steps.FOUR));
+    } else {
+      if (errors.length === 0) {
+        if (!guardianName) errors.push('보호자 이름');
+        if (!guardianPhoneNumber) errors.push('보호자 연락처');
+        if (!relationship) errors.push('보호자 관계');
+        if (!schoolTeacherName) errors.push('담임선생님 이름');
+        if (!schoolTeacherPhoneNumber) errors.push('담임선생님 연락처');
+      }
+
+      setShowModal(true);
     }
   };
 
@@ -200,46 +241,65 @@ const StepBar = ({ param, handleSubmit, watch }: StepBarType) => {
 
   const handlePrevious = () => {
     const prevStep = Math.max(currentStep - 1, Steps.ONE);
+    setErrors([]);
     updateStep(prevStep);
   };
 
   return (
-    <div
-      className={cn(
-        'flex',
-        'h-[4.25rem]',
-        'px-[1.75rem]',
-        'py-[1.125rem]',
-        'justify-between',
-        'items-center',
-        'rounded-t-[1.125rem]',
-        'bg-white',
-        'border-solid',
-        'border-b',
-        'border-gray-100',
-      )}
-    >
-      <div className={cn('flex', 'items-center', 'gap-[0.5rem]')}>
-        {[Steps.ONE, Steps.TWO, Steps.THREE, Steps.FOUR].map((step, index) => (
-          <div key={step} className={cn('flex', 'items-center', 'gap-[0.5rem]')}>
-            <Step step={step} isActive={currentStep === step} isCompleted={currentStep > step} />
-            {index < Steps.FOUR - 1 && (
-              <ProgressBarIcon color={currentStep > step ? '#2563eb' : '#CBD5E1'} />
-            )}
-          </div>
-        ))}
-      </div>
-      <div className={cn('flex', 'gap-[0.5rem]')}>
-        {currentStep > Steps.ONE && (
-          <Button variant="ghost" onClick={handlePrevious}>
-            이전
-          </Button>
+    <>
+      <div
+        className={cn(
+          'flex',
+          'h-[4.25rem]',
+          'px-[1.75rem]',
+          'py-[1.125rem]',
+          'justify-between',
+          'items-center',
+          'rounded-t-[1.125rem]',
+          'bg-white',
+          'border-solid',
+          'border-b',
+          'border-gray-100',
         )}
-        <Button variant="submit" onClick={handleNext}>
-          {currentStep === Steps.FOUR ? '점수 확인하기' : '다음으로'}
-        </Button>
+      >
+        <div className={cn('flex', 'items-center', 'gap-[0.5rem]')}>
+          {[Steps.ONE, Steps.TWO, Steps.THREE, Steps.FOUR].map((step, index) => (
+            <div key={step} className={cn('flex', 'items-center', 'gap-[0.5rem]')}>
+              <Step step={step} isActive={currentStep === step} isCompleted={currentStep > step} />
+              {index < Steps.FOUR - 1 && (
+                <ProgressBarIcon color={currentStep > step ? '#2563eb' : '#CBD5E1'} />
+              )}
+            </div>
+          ))}
+        </div>
+        <div className={cn('flex', 'gap-[0.5rem]')}>
+          {currentStep > Steps.ONE && (
+            <Button variant="ghost" onClick={handlePrevious}>
+              이전
+            </Button>
+          )}
+          <Button variant="submit" onClick={handleNext}>
+            {currentStep === Steps.FOUR ? '점수 확인하기' : '다음으로'}
+          </Button>
+        </div>
       </div>
-    </div>
+      <AlertDialog open={showModal}>
+        <AlertDialogContent className="w-[400px]">
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              필수 입력 부분 중
+              <p className={cn('text-red-600', 'text-[0.875rem]/[1.25rem]', 'font-normal')}>
+                {errors.filter(Boolean).join(', ')}
+              </p>
+              부분이 채워지지 않았습니다.
+            </AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction onClick={() => setShowModal(false)}>확인</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
