d3a18433feff8804764b44fdb043858678f0b9e3
gaoooon <gaon9749@gmail.com>
2024-08-30 22:14:12 +0900

    fix build error

--- apps/admin/src/pageContainer/Edit/Step4Page/index.tsx
+++ apps/admin/src/pageContainer/Edit/Step4Page/index.tsx
@@ -14,7 +14,8 @@ const EditStep4Page = ({ data, memberId }: EditStep4PageProps) => {
       <div className={cn('w-full', 'flex', 'justify-center')}>
         <div className={cn('flex', 'flex-col')}>
           {/* <StepBar /> */}
-          <ScoreRegister type="admin" data={data} memberId={memberId} />
+          <ScoreRegister type="admin" data={data} memberId={memberId} isStep4Checkable={false} />
+          {/* isStep4Checkable={false} 부분은 다음 pr에서 수정하겠습니다 */}
         </div>
       </div>
     </>
