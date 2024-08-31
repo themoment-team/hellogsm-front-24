05829249e7cb37b4fb579c733eaaf95583938e17
gjaegyun <jaegyungim06@gmail.com>
2024-08-31 18:17:26 +0900

    fix: 오타 변경

--- packages/api/src/libs/queryKeys.ts
+++ packages/api/src/libs/queryKeys.ts
@@ -22,7 +22,7 @@ export const oneseoQueryKeys = {
   patchAptitudeScore: (memberId: number) => ['patch', 'aptitudeScore', memberId],
   patchInterviewScore: (memberId: number) => ['patch', 'interviewScore', memberId],
   getAdmissionTickets: () => ['tickets'],
-  getMyOneseo: () => ['ge', 'my', 'oneseo'],
+  getMyOneseo: () => ['get', 'my', 'oneseo'],
 } as const;
 
 export const memberQueryKeys = {
