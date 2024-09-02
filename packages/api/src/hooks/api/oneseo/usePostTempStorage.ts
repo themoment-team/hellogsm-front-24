fad506f23274fa772d7f098d410bc3fedb8272f0
gaoooon <gaon9749@gmail.com>
2024-09-02 19:06:30 +0900

    Add step

--- packages/api/src/hooks/api/oneseo/usePostTempStorage.ts
+++ packages/api/src/hooks/api/oneseo/usePostTempStorage.ts
@@ -7,10 +7,11 @@ import { PostOneseoType } from 'types';
 import { oneseoQueryKeys, oneseoUrl, post } from 'api/libs';
 
 export const usePostTempStorage = (
+  step: number,
   options?: UseMutationOptions<unknown, AxiosError, PostOneseoType>,
 ) =>
   useMutation({
     mutationKey: oneseoQueryKeys.postTempStorage(),
-    mutationFn: (data: PostOneseoType) => post(oneseoUrl.postTempStorage(), data),
+    mutationFn: (data: PostOneseoType) => post(oneseoUrl.postTempStorage(step), data),
     ...options,
   });
