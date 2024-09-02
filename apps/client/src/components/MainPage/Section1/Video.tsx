a6d2b87cacccf8e8a290da5625f313fd140e2e4b
gaoooon <gaon9749@gmail.com>
2024-09-02 11:52:03 +0900

    Update poster

--- apps/client/src/components/MainPage/Section1/Video.tsx
+++ apps/client/src/components/MainPage/Section1/Video.tsx
@@ -1,7 +1,14 @@
 import { cn } from 'shared/lib/utils';
 
 const Video = () => (
-  <video className={cn('w-full', 'h-full', 'object-cover')} autoPlay loop muted playsInline>
+  <video
+    className={cn('w-full', 'h-full', 'object-cover')}
+    poster="/images/promotion/poster.png"
+    autoPlay
+    loop
+    muted
+    playsInline
+  >
     <source src="/video/promotion.webm" type="video/webm" />
     <source src="/video/promotion.mp4" type="video/mp4" />
   </video>
