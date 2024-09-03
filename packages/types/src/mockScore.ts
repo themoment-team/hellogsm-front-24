94f5b5a790c06aafd6a7db605f64733f6b914ced
gaoooon <gaon9749@gmail.com>
2024-09-03 13:26:19 +0900

    Update type

--- packages/types/src/mockScore.ts
+++ packages/types/src/mockScore.ts
@@ -1,7 +1,7 @@
 export interface MockScoreType {
-  generalSubjectsScore: number;
-  artsPhysicalSubjectsScore: number;
-  totalSubjectScore: number;
+  generalSubjectsScore?: number;
+  artsPhysicalSubjectsScore?: number;
+  totalSubjectsScore?: number;
   attendanceScore: number;
   volunteerScore: number;
   totalScore: number;
