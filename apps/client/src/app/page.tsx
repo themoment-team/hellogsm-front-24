e38862fd9a2afa2284c7af9958289ae1ad94a2f9
gaoooon <gaon9749@gmail.com>
2024-09-03 20:33:41 +0900

    Update page.tsx

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -5,8 +5,7 @@ import { MainPage } from 'client/pageContainer';
 import { getMyAuthInfo, getMyMemberInfo } from './apis';
 
 export default async function Home() {
-  const memberInfo = await getMyMemberInfo('/');
-  const authInfo = await getMyAuthInfo('/');
+  const [memberInfo, authInfo] = await Promise.all([getMyMemberInfo('/'), getMyAuthInfo('/')]);
 
   if (authInfo?.authReferrerType && !memberInfo?.name) {
     redirect('/signup');
