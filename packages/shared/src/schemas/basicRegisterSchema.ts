a512ad5fe475d6ce1d77c3ff545ff53e17e103ef
gaoooon <gaon9749@gmail.com>
2024-09-08 20:18:04 +0900

    Update basicRegisterSchema

--- packages/shared/src/schemas/basicRegisterSchema.ts
+++ packages/shared/src/schemas/basicRegisterSchema.ts
@@ -19,11 +19,13 @@ export const basicRegisterSchema = z
       .max(11, { message: '보호자 전화번호를 제대로 입력해주세요.' }),
     relationship: z.string().min(1, '보호자와의 관계를 선택해주세요').optional(),
     otherRelationship: z.string().min(1, '보호자와의 관계를 입력해주세요').optional(),
-    schoolTeacherName: z.string().min(1, '선생님의 이름을 입력해주세요'),
-    schoolTeacherPhoneNumber: z
-      .string()
-      .min(1, '휴대폰 번호를 입력해주세요')
-      .max(11, { message: '선생님 전화번호를 제대로 입력해주세요.' }),
+    schoolTeacherName: z.nullable(z.string().min(1, '선생님의 이름을 입력해주세요')),
+    schoolTeacherPhoneNumber: z.nullable(
+      z
+        .string()
+        .min(1, '휴대폰 번호를 입력해주세요')
+        .max(11, { message: '선생님 전화번호를 제대로 입력해주세요.' }),
+    ),
   })
   .refine((data) => data.relationship || data.otherRelationship, {
     message: '보호자와의 관계를 선택하거나 입력해주세요.',
