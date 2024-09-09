78dbe259809ae64bb8c3c6fd221ba62ed064b87c
gjaegyun <jaegyungim06@gmail.com>
2024-09-09 17:40:24 +0900

    feat: prevent temp

--- apps/client/src/app/mypage/page.tsx
+++ apps/client/src/app/mypage/page.tsx
@@ -1,7 +1,11 @@
+import { redirect } from 'next/navigation';
+
 import { getMyOneseo } from 'client/app/apis';
 import { MyPage as MyPageComponent } from 'client/pageContainer';
 
 export default async function MyPage() {
   const data = await getMyOneseo();
+  redirect('/');
+  //TODO 임시 redirect
   return <MyPageComponent initialData={data} />;
 }
