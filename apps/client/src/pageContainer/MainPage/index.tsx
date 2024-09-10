d3bf18f8adcb31b93f73829f049db7fb72f99b71
gjaegyun <jaegyungim06@gmail.com>
2024-09-10 14:02:12 +0900

    feat: 하루동안 보지않기 feature 제작

--- apps/client/src/pageContainer/MainPage/index.tsx
+++ apps/client/src/pageContainer/MainPage/index.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { MyTestResultType } from 'types';
 
-import { Footer, Section5 } from 'client/components';
+import { Footer, PassResultDialog, Section5, TestResultDialog } from 'client/components';
 import { Section1, Section2, Section3, Section4 } from 'client/components';
 
 interface MainPageProps {
@@ -8,6 +12,22 @@ interface MainPageProps {
 }
 
 const MainPage = ({ resultInfo }: MainPageProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isPassOpen, setIsPassOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const today = new Date().toDateString();
+    const hideDialog = localStorage.getItem('hideTestResultDialog');
+
+    if (hideDialog === today) {
+      setIsOpen(false);
+    } else if (resultInfo) {
+      setIsOpen(true);
+    }
+  }, [resultInfo]);
+
+  const isFinishFirstTest = resultInfo?.secondTestPassYn === null ? true : false;
+
   return (
     <>
       <Section1 />
@@ -16,6 +36,18 @@ const MainPage = ({ resultInfo }: MainPageProps) => {
       <Section4 />
       <Section5 />
       <Footer />
+      <TestResultDialog
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        onOpenPassResultDialog={() => setIsPassOpen(true)}
+        isFinishFirstTest={isFinishFirstTest}
+      />
+      <PassResultDialog
+        isPassOpen={isPassOpen}
+        setIsPassOpen={setIsPassOpen}
+        resultInfo={resultInfo}
+        isFinishFirstTest={isFinishFirstTest}
+      />
     </>
   );
 };
