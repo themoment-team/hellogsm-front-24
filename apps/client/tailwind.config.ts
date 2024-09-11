b4d29cdb9a1bd20f5a0c28f6e9f12a5dcf7476d0
gjaegyun <jaegyungim06@gmail.com>
2024-09-11 17:30:57 +0900

    feat: customize px

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,1 +1,23 @@
-module.exports = require('tailwind-config/tailwind.config');
+const baseConfig = require('tailwind-config/tailwind.config');
+
+module.exports = {
+  ...baseConfig,
+  theme: {
+    ...baseConfig.theme,
+    screens: {
+      xs: '450px',
+
+      sm: '600px',
+
+      smx: '850px',
+
+      md: '1024px',
+
+      lg: '1440px',
+
+      xl: '1728px',
+
+      '2xl': '1920px',
+    },
+  },
+};
