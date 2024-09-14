2cfc3a11c3bc189690553977725b74e12f46a884
gjaegyun <jaegyungim06@gmail.com>
2024-09-14 23:41:40 +0900

    fix: 디자인 변경사항 반영

--- apps/client/src/components/MainPage/Section3/index.tsx
+++ apps/client/src/components/MainPage/Section3/index.tsx
@@ -35,9 +35,7 @@ const Section3 = () => {
           'uhd:px-[32.5rem]',
         )}
       >
-        <div
-          className={cn('text-left', 'flex', 'justify-center', 'lg:inline', 'smx:justify-between')}
-        >
+        <div className={cn('text-left', 'flex', 'lg:inline', 'flex-col')}>
           <h1
             className={cn(
               'font-semibold',
@@ -55,18 +53,15 @@ const Section3 = () => {
           </h1>
           <p
             className={cn(
-              'mb-[2rem]',
-              'mt-0',
-              'lg:mt-[1rem]',
+              'mb-0',
+              'lg:mb-[2rem]',
+              'mt-[1rem]',
               'hidden',
               'smx:flex',
               'text-gray-500',
             )}
           >
-            접수기간: {RECRUITMENT_PERIOD.startDate}
-            <br className={cn('inline', 'lg:hidden')} />
-            {' ~ '}
-            {RECRUITMENT_PERIOD.endDate}
+            접수기간: {RECRUITMENT_PERIOD.startDate} ~ {RECRUITMENT_PERIOD.endDate}
           </p>
           <Link
             href="/"
