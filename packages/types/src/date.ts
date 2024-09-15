a996c4a2622332f11db6171f9a27171eba4c3aaa
gaoooon <gaon9749@gmail.com>
2024-09-15 17:42:02 +0900

    fix : type 수정

--- packages/types/src/date.ts
+++ packages/types/src/date.ts
@@ -1,8 +1,8 @@
 export interface DateType {
-  oneseoSubmissionStart: "2024-09-14T16:31:07.325Z";
-  oneseoSubmissionEnd: "2024-09-14T16:31:07.325Z";
-  firstResultsAnnouncement: "2024-09-14T16:31:07.325Z";
-  jobFitAssessment: "2024-09-14T16:31:07.325Z";
-  inDepthInterview: "2024-09-14T16:31:07.325Z";
-  finalResultsAnnouncement: "2024-09-14T16:31:07.325Z";
+  oneseoSubmissionStart: string;
+  oneseoSubmissionEnd: string;
+  firstResultsAnnouncement: string;
+  jobFitAssessment: string;
+  inDepthInterview: string;
+  finalResultsAnnouncement: string;
 }
