df657f4a738e38043cfea0944d02e502f4ae728c
gaoooon <gaon9749@gmail.com>
2024-10-14 12:21:44 +0900

    fix : console 수정

--- apps/client/src/pageContainer/GuidePage/index.tsx
+++ apps/client/src/pageContainer/GuidePage/index.tsx
@@ -230,11 +230,15 @@ const GuidePage = ({ initialData, isOneseoWrite, dateList, currentTime }: GuideP
   console.log(isOneseoWrite);
 
   // eslint-disable-next-line no-console
-  console.log(`
-    ${new Date(dateList!.oneseoSubmissionStart).getTime()} <= currentTime &&
-      currentTime < ${new Date(dateList!.oneseoSubmissionEnd).getTime()}
-      ? true
-      : false`);
+  console.log(
+    dateList
+      ? `
+    dateList?.oneseoSubmissionStart && dateList?.oneseoSubmissionEnd
+    ? ${new Date(dateList.oneseoSubmissionStart).getTime()} <= currentTime &&
+      currentTime < ${new Date(dateList.oneseoSubmissionEnd).getTime()}
+    : false;`
+      : 'dateList 없음',
+  );
 
   const { data } = useGetMyOneseo({
     initialData: initialData,
