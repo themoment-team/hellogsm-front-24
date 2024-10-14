a8ad0e81b410416f5798bf31ae24c026c2d0111d
gaoooon <gaon9749@gmail.com>
2024-10-14 10:03:06 +0900

    fix : 임시 주석 해제

--- apps/client/src/app/register/page.tsx
+++ apps/client/src/app/register/page.tsx
@@ -11,16 +11,20 @@ interface RegisterProps {
 export default async function Register({ searchParams }: RegisterProps) {
   const step = searchParams?.step;
 
-  const [data, info] = await Promise.all([getMyOneseo(), getMyMemberInfo('/'), getDate()]);
+  const [data, info, dateList] = await Promise.all([
+    getMyOneseo(),
+    getMyMemberInfo('/'),
+    getDate(),
+  ]);
 
-  // const currentTime = new Date().getTime();
-  // const isOneseoWrite =
-  //   dateList?.oneseoSubmissionStart && dateList?.oneseoSubmissionEnd
-  //     ? new Date(dateList.oneseoSubmissionStart).getTime() <= currentTime &&
-  //       currentTime < new Date(dateList.oneseoSubmissionEnd).getTime()
-  //     : false;
+  const currentTime = new Date().getTime();
+  const isOneseoWrite =
+    dateList?.oneseoSubmissionStart && dateList?.oneseoSubmissionEnd
+      ? new Date(dateList.oneseoSubmissionStart).getTime() <= currentTime &&
+        currentTime < new Date(dateList.oneseoSubmissionEnd).getTime()
+      : false;
 
-  if (info === undefined || (data && !data.step)) redirect('/');
+  if (info === undefined || (data && !data.step) || !isOneseoWrite) redirect('/');
 
   if (!step) redirect('/register?step=1');
 
