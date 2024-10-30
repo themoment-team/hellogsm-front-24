5f89784e4886ac902bf1d1712d27563b41b7ab25
frorong <moondgod@gmail.com>
2024-10-30 11:13:44 +0900

    hotfix: 긴급 팝업 추가

--- apps/client/src/app/check-result/layout.tsx
+++ apps/client/src/app/check-result/layout.tsx
@@ -45,7 +45,12 @@ const Layout = ({
       <AlertDialog open={!authInfo?.authReferrerType || !memberInfo?.name}>
         <AlertDialogContent className="w-[400px]">
           <AlertDialogHeader>
-            <AlertDialogTitle>로그인을 먼저 진행해주세요</AlertDialogTitle>
+            <AlertDialogTitle>
+              학부모/ 담임교사 합격확인 시, 보안상의 문제로 본인확인을 위해 회원가입 후 학부모/
+              담임교사의 본인 로그인이 필요합니다. <br />
+              <br /> 빠른 확인을 원하시는 경우, 062-949-6842로 전화주시면 친절히 안내드리겠습니다.{' '}
+              <br /> 번거롭게 해드려 죄송합니다.
+            </AlertDialogTitle>
           </AlertDialogHeader>
           <AlertDialogFooter>
             <LoginDialog />
