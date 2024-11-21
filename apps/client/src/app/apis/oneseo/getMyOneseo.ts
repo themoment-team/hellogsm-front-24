0ca3e2998e7387c5ae0dc05f8f4e63e692d69e50
gaoooon <gaon9749@gmail.com>
2024-11-21 12:16:24 +0900

    fix : try catch로 에러시 undefined를 반환하도록 수정

--- apps/client/src/app/apis/oneseo/getMyOneseo.ts
+++ apps/client/src/app/apis/oneseo/getMyOneseo.ts
@@ -5,22 +5,23 @@ import { oneseoUrl } from 'api/libs';
 
 export const getMyOneseo = async (): Promise<GetMyOneseoType | undefined> => {
   const session = cookies().get('SESSION')?.value;
-
-  const response = await fetch(
-    new URL(oneseoUrl.getMyOneseo(), process.env.NEXT_PUBLIC_API_BASE_URL),
-    {
-      method: 'GET',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        Cookie: `SESSION=${session}`,
+  try {
+    const response = await fetch(
+      new URL(oneseoUrl.getMyOneseo(), process.env.NEXT_PUBLIC_API_BASE_URL),
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `SESSION=${session}`,
+        },
       },
-    },
-  );
+    );
 
-  const myOneseo = await response.json();
+    const myOneseo = await response.json();
 
-  if (response.status === 404) return undefined;
-
-  return myOneseo.data;
+    return myOneseo.data;
+  } catch {
+    return undefined;
+  }
 };
