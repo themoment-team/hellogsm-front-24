e1b80a41ab6271776afaf67b082980881c1c1b3d
gaoooon <gaon9749@gmail.com>
2025-01-09 01:08:34 +0900

    feat : MAX_SCORE, MIN_SCORE, GED_MAX_SCORE 상수 추가

--- packages/shared/src/constants/score.ts
+++ packages/shared/src/constants/score.ts
@@ -1,1 +1,15 @@
-export const scoreArray = ['A', 'B', 'C', 'D', 'E', '없음'] as const;
+import { AchievementValueEnum } from 'types';
+
+export const SCORE_VALUES = [
+  { name: 'A', value: AchievementValueEnum.A },
+  { name: 'B', value: AchievementValueEnum.B },
+  { name: 'C', value: AchievementValueEnum.C },
+  { name: 'D', value: AchievementValueEnum.D },
+  { name: 'E', value: AchievementValueEnum.E },
+  { name: '없음', value: AchievementValueEnum.NONE },
+] as const;
+
+export const MAX_SCORE = 5;
+export const MIN_SCORE = 0;
+
+export const GED_MAX_SCORE = 600;
