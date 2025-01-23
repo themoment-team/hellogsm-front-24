b391068c88281cd22a5bd5ca69407c166b32aee8
gjaegyun <jaegyungim06@gmail.com>
2025-01-23 16:45:13 +0900

    feat: noticeDialog 모듈화

--- apps/client/src/pageContainer/MainPage/index.tsx
+++ apps/client/src/pageContainer/MainPage/index.tsx
@@ -2,14 +2,12 @@
 
 import { useEffect, useState } from 'react';
 
-//TODO 추후 합격자 조회 기능의 변경에 따라 코드 수정
-
-// import { useGetMyAuthInfo } from 'api';
+import { useGetMyAuthInfo } from 'api';
 import { MyMemberInfoType, MyTotalTestResultType } from 'types';
 
 import {
   Footer,
-  // LoginDialog,
+  LoginNoticeDialog,
   PassResultDialog,
   Section1,
   Section2,
@@ -19,18 +17,7 @@ import {
   TestResultDialog,
 } from 'client/components';
 
-import {
-  // AlertDialog,
-  // AlertDialogContent,
-  // AlertDialogFooter,
-  // AlertDialogHeader,
-  // AlertDialogTitle,
-  // Button,
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from 'shared/components';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from 'shared/components';
 import { cn } from 'shared/lib/utils';
 
 interface MainPageProps {
@@ -43,7 +30,7 @@ const MainPage = ({ memberInfo, resultInfo, isServerHealthy }: MainPageProps) =>
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isPassOpen, setIsPassOpen] = useState<boolean>(false);
   const [isStage, setIsStage] = useState<boolean>(false);
-  // const [isClicked, setIsClicked] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     const today = new Date().toDateString();
@@ -66,29 +53,16 @@ const MainPage = ({ memberInfo, resultInfo, isServerHealthy }: MainPageProps) =>
 
   const isFinishFirstTest = resultInfo?.secondTestPassYn === null ? true : false;
 
-  // const { data: authInfo } = useGetMyAuthInfo();
+  const { data: authInfo } = useGetMyAuthInfo();
 
   return (
     <>
-      {/* <AlertDialog open={!isClicked && (!authInfo?.authReferrerType || !memberInfo?.name)}>
-        <AlertDialogContent className="w-[400px]">
-          <AlertDialogHeader>
-            <AlertDialogTitle>
-              <strong>로그인을 먼저 진행해주세요</strong>
-              <br />
-              <br />
-              학부모/ 담임교사 합격확인 시, 보안상의 문제로 본인확인을 위해 회원가입 후 학부모/
-              담임교사의 본인 로그인이 필요합니다. <br />
-              <br /> 빠른 확인을 원하시는 경우, 062-949-6842로 전화주시면 친절히 안내드리겠습니다.{' '}
-              <br /> 번거롭게 해드려 죄송합니다.
-            </AlertDialogTitle>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <LoginDialog />
-            <Button onClick={() => setIsClicked(true)}>다음에</Button>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog> */}
+      <LoginNoticeDialog
+        isClicked={isClicked}
+        setIsClicked={setIsClicked}
+        authInfo={authInfo}
+        memberInfo={memberInfo}
+      />
       <Section1 />
       <Section2 />
       <Section3 isServerHealthy={isServerHealthy} />
