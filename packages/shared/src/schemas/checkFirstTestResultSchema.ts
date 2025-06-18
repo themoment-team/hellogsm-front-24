fbf675d23e49bf7081af8ffd2d7f3bf4dd35e0cd
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-06-18 15:28:48 +0900

    feat: 요구사항에 맞춰 합격자 조회 폼 스키마 변경

--- packages/shared/src/schemas/checkFirstTestResultSchema.ts
+++ packages/shared/src/schemas/checkFirstTestResultSchema.ts
@@ -1,11 +1,15 @@
 import { z } from 'zod';
 
-import { phoneNumberRegex } from 'shared/constants';
+import { koreanNameRegex, phoneNumberRegex } from 'shared/constants';
+
+import { birthSchema } from './birthSchema';
 
 export const checkFirstTestResultSchema = z.object({
   phoneNumber: z
     .string()
     .refine((phoneNumber) => phoneNumberRegex.test(phoneNumber), '전화번호 형식에 맞지 않습니다.'),
-  code: z.string().length(6, '인증번호 형식에 맞지 않습니다.'),
-  submitCode: z.string(),
+  name: z.string().refine((val) => koreanNameRegex.test(val), {
+    message: '이름은 한글 2글자 이상이어야 합니다.',
+  }),
+  birth: birthSchema,
 });
