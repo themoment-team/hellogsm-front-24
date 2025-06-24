75316716825ecd06b8beb4cf088a74921503f599
junjuny0227 <junjuny0227@gmail.com>
2025-06-24 16:16:24 +0900

    refactor: 변경된 파일 이름 반영

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -2,7 +2,7 @@ export { default as checkIsPassedDate } from './checkIsPassedDate';
 export { default as formatScore } from './formatScore';
 export { default as minutesToMs } from './minutesToMs';
 export { default as plusAll } from './plusAll';
-export { default as dataUrltoFile } from './dataUrltoFile';
+export { default as dataURLtoFile } from './dataURLtoFile';
 export { default as getValuesByEnum } from './getValuesByEnum';
 export * from './scroll';
 export { default as formattedBirthDate } from './formatBirth';
