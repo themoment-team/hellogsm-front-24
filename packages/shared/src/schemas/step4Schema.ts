a8ea82f23d5a9c2d407cb2f399be8661e84f035a
h-0y.28 <s24015@gsm.hs.kr>
2025-07-22 13:30:18 +0900

    fix: 자유학기제 + 1학년 성적이 존재 할 때freeSemester 값 필수 검증 로직 추가

--- packages/shared/src/schemas/step4Schema.ts
+++ packages/shared/src/schemas/step4Schema.ts
@@ -17,19 +17,32 @@ const achievementSchema = (minLength: number) =>
 
 const nonSubjectSchema = z.nullable(z.array(z.number().refine((value) => !isNaN(value))));
 
-export const step4Schema = z.object({
-  liberalSystem: z.nullable(z.enum(getValuesByEnum(LiberalSystemValueEnum))),
-  achievement1_1: achievementSchema(GENERAL_SUBJECTS.length),
-  achievement1_2: achievementSchema(GENERAL_SUBJECTS.length),
-  achievement2_1: achievementSchema(GENERAL_SUBJECTS.length),
-  achievement2_2: achievementSchema(GENERAL_SUBJECTS.length),
-  achievement3_1: achievementSchema(GENERAL_SUBJECTS.length),
-  achievement3_2: achievementSchema(GENERAL_SUBJECTS.length),
-  newSubjects: z.optional(z.array(z.string().min(1))),
-  artsPhysicalAchievement: achievementSchema(ARTS_PHYSICAL_SUBJECTS.length * 3),
-  absentDays: nonSubjectSchema,
-  attendanceDays: nonSubjectSchema,
-  volunteerTime: nonSubjectSchema,
-  freeSemester: z.nullable(z.enum(getValuesByEnum(FreeSemesterValueEnum))),
-  gedTotalScore: z.nullable(z.number().refine((value) => !isNaN(value) && value <= GED_MAX_SCORE)),
+export const step4Schema = z
+  .object({
+    liberalSystem: z.nullable(z.enum(getValuesByEnum(LiberalSystemValueEnum))),
+    achievement1_1: achievementSchema(GENERAL_SUBJECTS.length),
+    achievement1_2: achievementSchema(GENERAL_SUBJECTS.length),
+    achievement2_1: achievementSchema(GENERAL_SUBJECTS.length),
+    achievement2_2: achievementSchema(GENERAL_SUBJECTS.length),
+    achievement3_1: achievementSchema(GENERAL_SUBJECTS.length),
+    achievement3_2: achievementSchema(GENERAL_SUBJECTS.length),
+    newSubjects: z.optional(z.array(z.string().min(1))),
+    artsPhysicalAchievement: achievementSchema(ARTS_PHYSICAL_SUBJECTS.length * 3),
+    absentDays: nonSubjectSchema,
+    attendanceDays: nonSubjectSchema,
+    volunteerTime: nonSubjectSchema,
+    freeSemester: z.nullable(z.enum(getValuesByEnum(FreeSemesterValueEnum))),
+    gedTotalScore: z.nullable(z.number().refine((value) => !isNaN(value) && value <= GED_MAX_SCORE)),
+  })
+  .superRefine((data, ctx) => {
+  if (data.liberalSystem === LiberalSystemValueEnum.FREE_SEMESTER) {
+    const hasAchievement1_1 = data.achievement1_1 && data.achievement1_1.some((score) => score !== null && score !== undefined);
+    const hasAchievement1_2 = data.achievement1_2 && data.achievement1_2.some((score) => score !== null && score !== undefined);
+
+    if ((hasAchievement1_1 || hasAchievement1_2) && !data.freeSemester) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+      });
+    }
+  }
 });
