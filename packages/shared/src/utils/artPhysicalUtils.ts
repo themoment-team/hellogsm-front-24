922aa5984a659d434d6190c01606565580be3669
h-0y.28 <s24015@gsm.hs.kr>
2025-07-22 14:48:14 +0900

    feat: artPhysical 관련 유틸 함수 추가

---
+++ packages/shared/src/utils/artPhysicalUtils.ts
@@ -0,0 +1,39 @@
+import {
+  artPhysicalCandidateFreeGradeIndexArray,
+  artPhysicalCandidateFreeSemesterArray,
+  artPhysicalCandidateFreeSemesterIndexArray,
+  artPhysicalCandidateFreeYearArray,
+  artPhysicalGraduationArray,
+  artPhysicalGraduationIndexArray,
+} from 'shared/constants/ artPhysical';
+import { GraduationTypeValueEnum } from 'types';
+
+interface GetArtPhysicalArraysParams {
+  graduationType: GraduationTypeValueEnum;
+  isFreeSemester: boolean;
+  isFreeGrade: boolean;
+}
+
+export function getArtPhysicalArray({
+  graduationType,
+  isFreeSemester,
+  isFreeGrade,
+}: GetArtPhysicalArraysParams) {
+  if (graduationType === GraduationTypeValueEnum.CANDIDATE) {
+    if (isFreeSemester) return artPhysicalCandidateFreeSemesterArray;
+    if (isFreeGrade) return artPhysicalCandidateFreeYearArray;
+  }
+  return artPhysicalGraduationArray;
+}
+
+export function getArtPhysicalIndexArray({
+  graduationType,
+  isFreeSemester,
+  isFreeGrade,
+}: GetArtPhysicalArraysParams) {
+  if (graduationType === GraduationTypeValueEnum.CANDIDATE) {
+    if (isFreeSemester) return artPhysicalCandidateFreeSemesterIndexArray;
+    if (isFreeGrade) return artPhysicalCandidateFreeGradeIndexArray;
+  }
+  return artPhysicalGraduationIndexArray;
+}
