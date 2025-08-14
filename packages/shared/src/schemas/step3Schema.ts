856d3949bac8971ed854bbf93e8048c52e7a6255
h-0y.28 <s24015@gsm.hs.kr>
2025-08-14 19:55:11 +0900

    fix: 보호자 및 담임교사 전화번호 정규 표현식 수정

--- packages/shared/src/schemas/step3Schema.ts
+++ packages/shared/src/schemas/step3Schema.ts
@@ -1,17 +1,17 @@
 import { RelationshipWithGuardianValueEnum } from 'types';
 import { z } from 'zod';
 
-import { phoneNumberRegex } from 'shared/constants';
+import { guardianPhoneNumberRegex, teacherPhoneNumberRegex } from 'shared/constants';
 import { getValuesByEnum } from 'shared/utils';
 
 export const step3Schema = z
   .object({
     guardianName: z.string().min(1),
-    guardianPhoneNumber: z.string().regex(phoneNumberRegex),
+    guardianPhoneNumber: z.string().regex(guardianPhoneNumberRegex),
     relationshipWithGuardian: z.enum(getValuesByEnum(RelationshipWithGuardianValueEnum)),
     otherRelationshipWithGuardian: z.nullable(z.string().min(1)),
     schoolTeacherName: z.nullable(z.string().min(1)),
-    schoolTeacherPhoneNumber: z.nullable(z.string().regex(phoneNumberRegex)),
+    schoolTeacherPhoneNumber: z.nullable(z.string().regex(teacherPhoneNumberRegex)),
   })
   .refine(
     // relationshipWithGuardian의 값이 '기타'라면 otherRelationshipWithGuardian에 값이 있어야 한다는 식
