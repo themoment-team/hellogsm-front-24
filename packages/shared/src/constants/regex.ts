d1c4d1b9c0048583a7ccfd214b85fb525d74142d
h-0y.28 <s24015@gsm.hs.kr>
2025-08-14 19:54:47 +0900

    feat: 전화번호 정규 표현식 추가(보호자, 담임교사)

--- packages/shared/src/constants/regex.ts
+++ packages/shared/src/constants/regex.ts
@@ -1,2 +1,4 @@
 export const phoneNumberRegex = /^[0-9]{11}$/;
+export const guardianPhoneNumberRegex = /^010\d{8}$/;
+export const teacherPhoneNumberRegex = /^0\d{8,10}$/;
 export const koreanNameRegex = /^[가-힣]{2,}$/;
