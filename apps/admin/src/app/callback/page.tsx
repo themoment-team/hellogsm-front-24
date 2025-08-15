31be1e6f41d2bf08ce5d8e2ca7d005112d89336a
junjuny0227 <junjuny0227@gmail.com>
2025-08-15 17:26:00 +0900

    chore: admin 콜백 제거

--- apps/admin/src/app/callback/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CallbackPage } from 'admin/pageContainer';
-
-export default function Callback({
-  searchParams,
-}: {
-  searchParams: { code: string; state: string };
-}) {
-  return <CallbackPage code={searchParams.code} provider={searchParams.state} />;
-}
--- apps/admin/src/pageContainer/index.ts
+++ apps/admin/src/pageContainer/index.ts
@@ -1,4 +1,3 @@
 export { default as LoginPage } from './LoginPage';
 export { default as MainPage } from './MainPage';
 export { default as TicketPage } from './TicketPage';
-export { default as CallbackPage } from './CallbackPage';
