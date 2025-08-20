407e8f102da1348aacd76557408f408956ce9940
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-08-21 00:59:29 +0900

    feat: 로그인 경고 다이얼로그 컴포넌트화

--- apps/client/src/app/check-result/page.tsx
+++ apps/client/src/app/check-result/page.tsx
@@ -1,15 +1,5 @@
 import { getDate } from 'api';
-import Link from 'next/link';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  getKoreanDate,
-  isTimeAfter,
-} from 'shared';
+import { getKoreanDate, isTimeAfter } from 'shared';
 
 import {
   getMyAuthInfo,
@@ -17,10 +7,8 @@ import {
   getMyMemberInfo,
   getMySecondTestResult,
 } from 'client/app/apis';
-import { LoginDialog } from 'client/components';
+import { AlertLoginDialog } from 'client/components';
 import { CheckResultPage } from 'client/pageContainer';
-
-const mainUrl = '/';
 
 export default async function CheckResult() {
   const [memberInfo, authInfo, firstResultInfo, secondResultInfo] = await Promise.all([
@@ -62,28 +50,7 @@ export default async function CheckResult() {
         isCheckFirstResult={isCheckFirstResult}
         isCheckFinalResult={isCheckFinalResult}
       />
-      <AlertDialog open={!authInfo?.authReferrerType || !memberInfo?.name}>
-        <AlertDialogContent className="w-[400px]">
-          <AlertDialogHeader>
-            <AlertDialogTitle>
-              <strong>로그인을 먼저 진행해주세요</strong>
-              <br />
-              <br />
-              학부모/담임교사 합격 확인 시, 보안상의 문제로 <br />
-              본인 확인을 위해 회원가입 후 학부모/담임교사의 <br />
-              본인 로그인이 필요합니다. <br />
-              <br /> 빠른 확인을 원하시는 경우, 062-949-6842로 전화 주시면 친절히 안내해
-              드리겠습니다. <br /> 번거롭게 해드려 죄송합니다.
-            </AlertDialogTitle>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <LoginDialog />
-            <AlertDialogAction>
-              <Link href={mainUrl}>다음에</Link>
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <AlertLoginDialog open={!authInfo?.authReferrerType || !memberInfo?.name} />
     </>
   );
 }
--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -12,3 +12,4 @@ export { default as FaqElement } from './FaqElement';
 export { default as TestResultDialog } from './TestResultDialog';
 export { default as PassResultDialog } from './PassResultDialog';
 export { default as LoginNoticeDialog } from './LoginNoticeDialog';
+export { default as AlertLoginDialog } from './AlertLoginDialog';
