ae9ddb6d6a210d53674833e084b7bc297387382e
h-0y.28 <s24015@gsm.hs.kr>
2025-08-20 09:56:01 +0900

    feat: 서버 상태에 따른 모의 성적 안내 모달 추가

--- apps/client/src/app/oneseo/calculate/page.tsx
+++ apps/client/src/app/oneseo/calculate/page.tsx
@@ -1,5 +1,8 @@
 import { CalculatePage } from 'client/pageContainer';
+import { getIsServerHealthy } from 'client/utils';
 
-export default function Calculate() {
-  return <CalculatePage />;
+export default async function Calculate() {
+  const isServerHealthy = await getIsServerHealthy();
+
+  return <CalculatePage isServerHealthy={isServerHealthy} />;
 }
