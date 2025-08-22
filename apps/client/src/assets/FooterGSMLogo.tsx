a96b0512f1dffdb50aabb946d513bb699aefc29e
h-0y.28 <s24015@gsm.hs.kr>
2025-08-22 12:11:52 +0900

    style: FooterGSMLogo에 mt-auto 속성 추가

--- apps/client/src/assets/FooterGSMLogo.tsx
+++ apps/client/src/assets/FooterGSMLogo.tsx
@@ -5,7 +5,7 @@ const FooterGSMLogo = () => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 231 141"
     fill="none"
-    className={cn('w-[9rem]', 'h-auto', 'md:w-[14.44rem]')}
+    className={cn('w-[9rem]', 'h-auto', 'lg:w-[14.44rem]', 'mt-auto')}
   >
     <g clipPath="url(#clip0_2582_13103)">
       <path
