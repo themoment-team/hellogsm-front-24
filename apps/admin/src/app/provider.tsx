5a3c8b46425242e99730871564dafb2cac6d1172
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-08-25 20:04:35 +0900

    fix: 사용하지 않는 import 제거

--- apps/admin/src/app/provider.tsx
+++ apps/admin/src/app/provider.tsx
@@ -5,7 +5,6 @@ import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const Provider = ({ children }: PropsWithChildren) => {
   const queryClient = new QueryClient();
