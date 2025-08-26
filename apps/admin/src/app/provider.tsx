5e29325d8842acf582766ca7367271ed2746a14f
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-08-26 10:24:35 +0900

    feat: import 추가

--- apps/admin/src/app/provider.tsx
+++ apps/admin/src/app/provider.tsx
@@ -5,6 +5,7 @@ import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Provider = ({ children }: PropsWithChildren) => {
   const queryClient = new QueryClient();
